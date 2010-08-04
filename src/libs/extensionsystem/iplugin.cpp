912db7e4f87f897e2ef60291883eba655ace5c66
ABBAPOH <ABBAPOH@nextmail.ru>
2010-08-04 22:55:42 +0400

    Added documentation.

--- src/libs/extensionsystem/iplugin.cpp
+++ src/libs/extensionsystem/iplugin.cpp
@@ -24,6 +24,28 @@ IPlugin::~IPlugin()
 }
 
 /*!
+    \fn bool IPlugin::initialize()
+    \brief Function for primary initialization of plugin.
+    This function called after all dependencies are initialized.
+*/
+
+/*!
+    \fn void extensionsInitialized()
+    \brief Function for secondary initialization of plugin.
+    This function called after both the IPlugin::initialize() and IPlugin::extensionsInitialized()
+    methods of plugins that depend on this plugin have been called.
+*/
+
+/*!
+    \fn QString property(Properties property)
+    \brief Returns string propertis of the plugin.
+    Information from this function is stored in PluginSpec class.
+    This is place where you have to place all string information for plugin.
+    \sa PluginSpec
+    \sa IPlugin::Properties
+*/
+
+/*!
     \fn void addObject(QObject * object)
     \brief Adds \a object to object pool in PluginManager.
 */
