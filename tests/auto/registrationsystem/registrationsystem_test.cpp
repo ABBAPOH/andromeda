a5342cfee8c6708f4e1669241a688e236fe046bb
ABBAPOH <ABBAPOH@nextmail.ru>
2010-08-21 21:40:27 +0400

    Added auto test files

---
+++ tests/auto/registrationsystem/registrationsystem_test.cpp
@@ -0,0 +1,12 @@
+
+#include <QtTest/QtTest>
+#include <QObject>
+
+class TestRegistrationSystem: public QObject
+{
+    Q_OBJECT
+private slots:
+};
+
+ QTEST_MAIN(TestRegistrationSystem)
+ #include "registrationsystem_test.moc"
