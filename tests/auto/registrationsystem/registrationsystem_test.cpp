34b4043387341669b50a7207b6bba17d78ccb450
Ivan Komissarov <ABBAPOH@me.com>
2010-08-21 21:54:44 +0400

    Added registrationsystem test

--- tests/auto/registrationsystem/registrationsystem_test.cpp
+++ tests/auto/registrationsystem/registrationsystem_test.cpp
@@ -1,12 +1,53 @@
 
 #include <QtTest/QtTest>
 #include <QObject>
+#include <registrationmanager.h>
+#include <QString>
+
+using namespace RegistrationSystem;
+
+class Object: public QObject
+{
+    Q_OBJECT
+public:
+};
+
+class Registrator: public IRegistrator
+{
+public:
+    virtual bool canRegister(QObject *object)
+    {
+        return qobject_cast<Object*>(object);
+    }
+    virtual bool registerObject(QObject *object)
+    {
+        object->setObjectName("registered");
+    };
+};
 
 class TestRegistrationSystem: public QObject
 {
     Q_OBJECT
+    RegistrationManager *manager;
+    Registrator *registrator;
 private slots:
+    void initTestCase();
+    void test1();
 };
 
- QTEST_MAIN(TestRegistrationSystem)
- #include "registrationsystem_test.moc"
+void TestRegistrationSystem::initTestCase()
+{
+    manager = new RegistrationManager();
+    registrator = new Registrator();
+}
+
+void TestRegistrationSystem::test1()
+{
+    manager->addRegistrator(registrator, "test");
+    Object *object1 = new Object;
+    manager->registerObject(object1, "test");
+    QCOMPARE(object1->objectName(), QString("registered"));
+}
+
+QTEST_MAIN(TestRegistrationSystem)
+#include "registrationsystem_test.moc"
