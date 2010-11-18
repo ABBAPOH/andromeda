93282cba02dc8dcd3b57bb8cebe939b7c8140905
ABBAPOH <ABBAPOH@nextmail.ru>
2010-11-18 04:37:35 +0300

    Added openEditor function to EditorManager

--- src/plugins/coreplugin/editormanager/editormanager.h
+++ src/plugins/coreplugin/editormanager/editormanager.h
@@ -21,6 +21,7 @@ public:
     explicit EditorManager(QObject *parent = 0);
     ~EditorManager();
 
+    IEditor *openEditor(const QUrl &url);
     QList<IEditor *>openEditors(const QUrl &url);
 
 signals:
