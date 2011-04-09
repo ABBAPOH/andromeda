a49009982ba412343c2a482385433826f2184c3f
ABBAPOH <ABBAPOH@nextmail.ru>
2011-04-10 00:38:50 +0400

    Now options handle only geometry of a view

--- src/libs/guisystem/viewoptions.h
+++ src/libs/guisystem/viewoptions.h
@@ -13,16 +13,30 @@ class ViewOptionsData;
 class GUISYSTEM_EXPORT ViewOptions
 {
 public:
-    explicit ViewOptions(int area = 0);
+    ViewOptions();
+    explicit ViewOptions(const QString &id);
+    ViewOptions(const QString &id, int area, int width = 0, int height = 0);
 
     ViewOptions(const ViewOptions &);
     ViewOptions &operator=(const ViewOptions &);
     ~ViewOptions();
 
+    QString id() const;
+    void setId(const QString &id);
+
     int area() const;
+    void setArea(int area);
+
+    int width() const;
+    void setWidth(int width);
+
+    int height() const;
+    void setHeight(int height);
 
     QVariant option(const QString &option) const;
     void setOption(const QString &option, const QVariant &value);
+
+    QVariantHash toHash() const;
 
 private:
     QSharedDataPointer<ViewOptionsData> data;
