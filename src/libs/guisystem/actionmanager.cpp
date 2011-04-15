d766d2c7adab224981fed35912a44e76383f9fef
Ivan Komissarov <ABBAPOH@me.com>
2011-04-15 09:23:40 +0400

    Added some documentation

--- src/libs/guisystem/actionmanager.cpp
+++ src/libs/guisystem/actionmanager.cpp
@@ -142,6 +142,17 @@ QMenu * ActionManager::menu(const QString &id) const
     return d->mapToMenu.value(id);
 }
 
+/*!
+    \fn void ActionManager::registerAction(QAction *action, const QString &id)
+    \brief Register \a action in ActionManager.
+
+    Registering of action allows you to use ActionManager::connect methods to connect to actions by
+    ids, not direct pointers. You must provied unique id for each action you want to use.
+
+    Also, you can retrieve action using ActionManager::action method to specify some properties.
+
+    \sa ActionManager::registerMenu, ActionManager::unregister
+*/
 void ActionManager::registerAction(QAction *action, const QString &id)
 {
     Q_D(ActionManager);
@@ -155,6 +166,15 @@ void ActionManager::registerAction(QAction *action, const QString &id)
     }
 }
 
+/*!
+    \fn void ActionManager::registerMenu(QMenu *menu, const QString &id)
+    \brief Register \a menu in ActionManager.
+
+    Allows retrieving menu using ActionManager::menu method. Also registers QMenu::menuAction with same
+    \a id in manager. Returns menuAction.
+
+    \sa ActionManager::registerAction, ActionManager::unregister
+*/
 void ActionManager::registerMenu(QMenu *menu, const QString &id)
 {
     Q_D(ActionManager);
@@ -163,6 +183,40 @@ void ActionManager::registerMenu(QMenu *menu, const QString &id)
     registerAction(menu->menuAction(), id);
 }
 
+/*!
+    \fn void ActionManager::unregister(const QString &id)
+    \brief Unregister action or menu with \a id.
+
+    Removes all internal data of action or menu with \id from ActionManager. All connections are also
+    disconnected.
+
+    \sa ActionManager::registerAction, ActionManager::registerMenu
+*/
+void ActionManager::unregister(const QString &id)
+{
+    Q_D(ActionManager);
+
+    d->mapToMenu.remove(id);
+    QAction *action = d->mapToAction.take(id);
+    if (action) {
+        action->setProperty("id", QVariant());
+        QObject::disconnect(action,  SIGNAL(triggered(bool)),
+                            d, SLOT(onTrigger(bool)));
+    }
+}
+
+/*!
+    \fn bool ActionManager::connect(const QString &actionId, const QString &viewId, const char *slot)
+    \brief Connects action with \a actionId to \a slot in \a viewId.
+
+    This function connects QAction::triggered(bool) signal of action specified by \a actionId
+    to slot in any view with id \a viewId. While changing perspectives, actions will be automatically
+    enabled and disabled if view with specified id is present in current perspective, or not. In fact,
+    there is no QObject::connect calls in this methods, so you don't warn about lot of connections -
+    receiver is determined only on triggering action, which is rather fast.
+
+    \sa ActionManager::disconnect
+*/
 bool ActionManager::connect(const QString &actionId, const QString &viewId, const char *slot)
 {
     Q_D(ActionManager);
@@ -177,6 +231,17 @@ bool ActionManager::connect(const QString &actionId, const QString &viewId, cons
     return true;
 }
 
+/*!
+    \fn bool ActionManager::connect(const QString &actionId, QObject *receiver, const char *slot)
+    \brief Connects action with \a actionId directly to \a slot in \a receiver.
+
+    This function connects QAction::triggered(bool) signal of action specified by \a actionId
+    to slot in \a receiver. Action is always enabled and you should manually enable/disable it when
+    needed. You can also manually connect ot action using QObject::connect method to emulate behavior
+    of this function.
+
+    \sa ActionManager::action, ActionManager::disconnect
+*/
 bool ActionManager::connect(const QString &actionId, QObject *receiver, const char *slot)
 {
     Q_D(ActionManager);
@@ -195,6 +260,16 @@ bool ActionManager::connect(const QString &actionId, QObject *receiver, const ch
     return true;
 }
 
+/*!
+    \fn bool ActionManager::connect(const QString &actionId, const char *slot)
+    \brief Connects action with \a actionId to any widget that have method named \a slot.
+
+    This function connects QAction::triggered(bool) signal of action specified by \a actionId
+    to slot in widget. Actions enabled and disabled on changing focus if current focus widget have
+    specified slot or not.
+
+    \sa ActionManager::disconnect
+*/
 bool ActionManager::connect(const QString &actionId, const char *slot)
 {
     Q_D(ActionManager);
@@ -208,6 +283,14 @@ bool ActionManager::connect(const QString &actionId, const char *slot)
     return true;
 }
 
+/*!
+    \fn void ActionManager::disconnect(const QString &actionId, const QString &viewId, const char *slot)
+    \brief Disconnects previously established connection.
+
+    Disconnects connection between action with id \a actionId and view with id \a viewId.
+
+    \sa ActionManager::connect
+*/
 void ActionManager::disconnect(const QString &actionId, const QString &viewId, const char *slot)
 {
     Q_D(ActionManager);
@@ -215,6 +298,14 @@ void ActionManager::disconnect(const QString &actionId, const QString &viewId, c
     d->connectionsViews.remove(actionId, QPair<QString, const char*>(viewId, slot));
 }
 
+/*!
+    \fn void ActionManager::disconnect(const QString &actionId, QObject *receiver, const char *slot)
+    \brief Disconnects previously established connection.
+
+    Disconnects connection between action with id \a actionId and \a receiver.
+
+    \sa ActionManager::connect
+*/
 void ActionManager::disconnect(const QString &actionId, QObject *receiver, const char *slot)
 {
     Q_D(ActionManager);
@@ -222,6 +313,14 @@ void ActionManager::disconnect(const QString &actionId, QObject *receiver, const
     d->connectionsObjects.remove(actionId, QPair<QObject*, const char*>(receiver, slot));
 }
 
+/*!
+    \fn void ActionManager::disconnect(const QString &actionId, const char *slot)
+    \brief Disconnects previously established connection.
+
+    Removes connection between action with id \a actionId and any widgets.
+
+    \sa ActionManager::connect
+*/
 void ActionManager::disconnect(const QString &actionId, const char *slot)
 {
     Q_D(ActionManager);
