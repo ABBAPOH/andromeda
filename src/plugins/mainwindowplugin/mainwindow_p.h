54e38a12ec663fafdf6f13d040ce3a388c2055bc
Ivan Komissarov <ABBAPOH@me.com>
2011-04-19 00:34:33 +0400

    MainWindowPrivate separated to it's own header

---
+++ src/plugins/mainwindowplugin/mainwindow_p.h
@@ -0,0 +1,18 @@
+#ifndef MAINWINDOW_P_H
+#define MAINWINDOW_P_H
+
+#include "mainwindow.h"
+
+#include <enteredlineedit.h>
+
+namespace MainWindowPlugin {
+
+class MainWindowPrivate
+{
+public:
+    EnteredLineEdit *lineEdit;
+};
+
+} // namespace MainWindowPlugin
+
+#endif // MAINWINDOW_P_H
