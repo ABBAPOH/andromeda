3e98bc3191adc16ee66bfd1a6d36675ccba7d855
Ivan Komissarov <ABBAPOH@me.com>
2011-04-19 01:08:52 +0400

    New MainWindow

--- src/plugins/mainwindowplugin/mainwindow.cpp
+++ src/plugins/mainwindowplugin/mainwindow.cpp
@@ -1,17 +1,39 @@
 #include "mainwindow.h"
 #include "mainwindow_p.h"
 
+#include <QtGui/QAction>
+#include <QDebug>
+
 using namespace MainWindowPlugin;
 
+void MainWindowPrivate::onTextEntered(const QString &path)
+{
+    qDebug() << "MainWindowPrivate::onTextEntered" << path;
+}
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    d_ptr(new MainWindowPrivate)
+    GuiSystem::MainWindow(parent),
+    d_ptr(new MainWindowPrivate(this))
 {
     Q_D(MainWindow);
 
     d->lineEdit = new EnteredLineEdit(this);
+    d->toolBar = new QToolBar(this);
 
+    QAction *backAction = new QAction(QIcon(":/images/icons/back.png"), "Back", this);
+    QAction *forwardAction = new QAction(QIcon(":/images/icons/forward.png"), "Forward", this);
+
+    d->toolBar->addAction(backAction);
+    d->toolBar->addAction(forwardAction);
+    d->toolBar->addSeparator();
+    d->toolBar->addWidget(d->lineEdit);
+
+    addToolBar(d->toolBar);
     setUnifiedTitleAndToolBarOnMac(true);
+
+    connect(d->lineEdit, SIGNAL(textEntered(QString)), d, SLOT(onTextEntered(QString)));
+
+    resize(640, 480);
 }
 
 MainWindow::~MainWindow()
--- src/plugins/mainwindowplugin/mainwindow_p.h
+++ src/plugins/mainwindowplugin/mainwindow_p.h
@@ -3,14 +3,24 @@
 
 #include "mainwindow.h"
 
+#include <QtGui/QToolBar>
 #include <enteredlineedit.h>
 
 namespace MainWindowPlugin {
 
-class MainWindowPrivate
+class MainWindowPrivate : public QObject
 {
+    Q_OBJECT
+    Q_DECLARE_PUBLIC(MainWindow)
+    MainWindow *q_ptr;
 public:
+    MainWindowPrivate(MainWindow *qq) : q_ptr(qq) {}
+
     EnteredLineEdit *lineEdit;
+    QToolBar *toolBar;
+
+public slots:
+    void onTextEntered(const QString &);
 };
 
 } // namespace MainWindowPlugin
