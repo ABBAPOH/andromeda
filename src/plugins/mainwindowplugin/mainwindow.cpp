c44e4263e18fc292071c43b5a89373437749663e
Ivan Komissarov <ABBAPOH@me.com>
2011-04-19 00:32:34 +0400

    Cleared MainWindow

--- src/plugins/mainwindowplugin/mainwindow.cpp
+++ src/plugins/mainwindowplugin/mainwindow.cpp
@@ -1,65 +1,29 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
-#include <editorview.h>
-#include <QLineEdit>
-#include <QToolButton>
 #include <enteredlineedit.h>
+
+namespace MainWindowPlugin {
+
+class MainWindowPrivate
+{
+public:
+};
+
+} // namespace MainWindowPlugin
 
 using namespace CorePlugin;
 using namespace MainWindowPlugin;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    d_ptr(new MainWindowPrivate)
 {
-    ui->setupUi(this);
+    Q_D(MainWindow);
 
     setUnifiedTitleAndToolBarOnMac(true);
-
-    m_lineEdit = new EnteredLineEdit(this);
-    ui->toolBar->addWidget(m_lineEdit);
-
-    m_newTabButton = new QToolButton(this);
-    m_newTabButton->setText(tr("New Tab"));
-    ui->tabWidget->setCornerWidget(m_newTabButton, Qt::TopRightCorner);
-
-    connect(m_lineEdit, SIGNAL(textEntered(QString)), SLOT(open(QString)));
-    connect(m_newTabButton, SIGNAL(clicked()), SLOT(newTab()));
-
-    connect(ui->actionBack, SIGNAL(triggered()), SLOT(back()));
-    connect(ui->actionForward, SIGNAL(triggered()), SLOT(forward()));
-
-    newTab();
 }
 
 MainWindow::~MainWindow()
 {
-    qDebug("~MainWindow");
-    delete ui;
-}
-
-void MainWindow::newTab()
-{
-    ui->tabWidget->addTab(new EditorView, "tab");
-}
-
-void MainWindow::open(const QString &path)
-{
-    view()->open(path);
-}
-
-void MainWindow::back()
-{
-    view()->back();
-}
-
-void MainWindow::forward()
-{
-    view()->forward();
-}
-
-EditorView *MainWindow::view()
-{
-    return qobject_cast<EditorView *>(ui->tabWidget->currentWidget());
+    delete d_ptr;
 }
--- src/plugins/mainwindowplugin/mainwindow.h
+++ src/plugins/mainwindowplugin/mainwindow.h
@@ -1,41 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-
-namespace Ui {
-    class MainWindow;
-}
-
-namespace CorePlugin {
-    class EditorView;
-}
-
-class QLineEdit;
-class QToolButton;
+#include <QtGui/QMainWindow>
 
 namespace MainWindowPlugin {
 
+class MainWindowPrivate;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    Q_DECLARE_PRIVATE(MainWindow)
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 public slots:
-    void newTab();
-    void open(const QString &);
-    void back();
-    void forward();
 
-private:
-    Ui::MainWindow *ui;
-    QLineEdit *m_lineEdit;
-    QToolButton *m_newTabButton;
-
-    CorePlugin::EditorView *view();
+protected:
+    MainWindowPrivate * d_ptr;
 };
 
 } // namespace MainWindowPlugin
