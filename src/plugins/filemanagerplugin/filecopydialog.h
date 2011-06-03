ea4707dc6625a45864a1f9333c2828732370c8d9
ABBAPOH <ABBAPOH@nextmail.ru>
2011-06-04 00:45:13 +0400

    FileCopyWidget moved to FileCopyDialog header

--- src/plugins/filemanagerplugin/filecopydialog.h
+++ src/plugins/filemanagerplugin/filecopydialog.h
@@ -4,7 +4,8 @@
 #include <QtGui/QDialog>
 
 namespace Ui {
-    class FileCopyReplaceDialog;
+    class FileCopyDialog;
+    class FileCopyWidget;
 }
 
 namespace FileManagerPlugin {
@@ -22,7 +23,29 @@ protected:
     void resizeEvent(QResizeEvent *);
 
 private:
-    Ui::FileCopyReplaceDialog *ui;
+    Ui::FileCopyDialog *ui;
+};
+
+class FileCopyTask;
+class FileCopyWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit FileCopyWidget(FileCopyTask *task, QWidget *parent = 0);
+    ~FileCopyWidget();
+
+signals:
+    void canceled();
+
+public slots:
+    void update();
+    void updateProgress(qint64 progress);
+//    void showDetails(bool show);
+
+private:
+    Ui::FileCopyWidget *ui;
+    FileCopyTask *m_task;
 };
 
 } // namespace FileManagerPlugin
