d571c2ae269fd43f1930431d0beb31bee24e680b
ABBAPOH <ABBAPOH@nextmail.ru>
2011-06-09 00:44:59 +0400

    Some fixes

--- src/plugins/coreplugin/page.h
+++ src/plugins/coreplugin/page.h
@@ -19,24 +19,27 @@ public:
     explicit Page(QObject *parent = 0);
 
     QString currentPath() const;
-    void setCurrentPath(QString currentPath);
 
     History *history() const;
 
     GuiSystem::State *state() const;
     void setState(GuiSystem::State *state);
 
-private slots:
-    void onIndexChanged(int index);
+public slots:
+    void setCurrentPath(QString currentPath);
 
 signals:
     void currentPathChanged(QString currentPath);
+
+private slots:
+    void onIndexChanged(int index);
 
 private:
     IMainView *getMainView(const QString &perspective);
     QString getPerspective(const QString &path);
     void openPerspective(const QString &path);
     void openPerspective(const HistoryItem &item);
+
     QString m_currentPath;
     History *m_history;
 };
