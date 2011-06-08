e9cf3a30a07982bf842621688fed061ef168d743
Ivan Komissarov <ABBAPOH@me.com>
2011-06-08 21:35:57 +0400

    Added some actions:)

--- src/plugins/mainwindowplugin/mainwindowplugin.cpp
+++ src/plugins/mainwindowplugin/mainwindowplugin.cpp
@@ -16,45 +16,70 @@ MainWindowPluginImpl::MainWindowPluginImpl() :
 
 bool MainWindowPluginImpl::initialize()
 {
-    window = new MainWindow();
-    window->show();
-
-    ActionManager *manager = ActionManager::instance();
-    manager->addMenu(new QMenu("File"));
-    QMenu *editMenu = new QMenu("Edit");
-    manager->addMenu(editMenu);
-    manager->registerMenu(editMenu, "MENU_EDIT");
-
-    QAction *undoAction = new QAction("Undo", manager);
-    undoAction->setShortcut(tr("Ctrl+Z"));
-    editMenu->addAction(undoAction);
-    manager->registerAction(undoAction, "ACTION_UNDO");
-
-    QAction *redoAction = new QAction("Redo", manager);
-    redoAction->setShortcut(tr("Ctrl+Shift+Z"));
-    editMenu->addAction(redoAction);
-    manager->registerAction(redoAction, "ACTION_REDO");
-
-    editMenu->addSeparator();
-
-    QAction *copyAction = new QAction("Copy", manager);
-    copyAction->setShortcut(tr("Ctrl+C"));
-    editMenu->addAction(copyAction);
-    manager->registerAction(copyAction, "ACTION_COPY");
-    manager->connect("ACTION_COPY", "copy()");
-
-    QAction *pasteAction = new QAction("Paste", manager);
-    pasteAction->setShortcut(tr("Ctrl+V"));
-    editMenu->addAction(pasteAction);
-    manager->registerAction(pasteAction, "ACTION_PASTE");
-    manager->connect("ACTION_PASTE", "paste()");
+    createActions();
+    newWindow();
 
     return true;
 }
 
 void MainWindowPluginImpl::shutdown()
 {
-    delete window;
+}
+
+void MainWindowPluginImpl::newWindow()
+{
+    MainWindow *window = new MainWindow();
+    window->show();
+    addObject(window);
+}
+
+void MainWindowPluginImpl::createActions()
+{
+    ActionManager *manager = ActionManager::instance();
+
+    QMenu *fileMenu = new QMenu("File");
+    manager->addMenu(fileMenu);
+    manager->registerMenu(fileMenu, MENU_FILE);
+
+    QAction *newWindowAction = new QAction("New window", manager);
+    newWindowAction->setShortcut(tr("Ctrl+N"));
+    fileMenu->addAction(newWindowAction);
+    manager->registerAction(newWindowAction, ACTION_NEW_WINDOW);
+    connect(newWindowAction, SIGNAL(triggered()), SLOT(newWindow()));
+
+    QMenu *editMenu = new QMenu("Edit");
+    manager->addMenu(editMenu);
+    manager->registerMenu(editMenu, MENU_EDIT);
+
+    QAction *undoAction = new QAction("Undo", manager);
+    undoAction->setShortcut(tr("Ctrl+Z"));
+    editMenu->addAction(undoAction);
+    manager->registerAction(undoAction, ACTION_UNDO);
+
+    QAction *redoAction = new QAction("Redo", manager);
+    redoAction->setShortcut(tr("Ctrl+Shift+Z"));
+    editMenu->addAction(redoAction);
+    manager->registerAction(redoAction, ACTION_REDO);
+
+    editMenu->addSeparator();
+
+    QAction *cutAction = new QAction("Cut", manager);
+    cutAction->setShortcut(tr("Ctrl+X"));
+    editMenu->addAction(cutAction);
+    manager->registerAction(cutAction, ACTION_CUT);
+    manager->connect("ACTION_CUT", "cut()");
+
+    QAction *copyAction = new QAction("Copy", manager);
+    copyAction->setShortcut(tr("Ctrl+C"));
+    editMenu->addAction(copyAction);
+    manager->registerAction(copyAction, ACTION_COPY);
+    manager->connect("ACTION_COPY", "copy()");
+
+    QAction *pasteAction = new QAction("Paste", manager);
+    pasteAction->setShortcut(tr("Ctrl+V"));
+    editMenu->addAction(pasteAction);
+    manager->registerAction(pasteAction, ACTION_PASTE);
+    manager->connect("ACTION_PASTE", "paste()");
 }
 
 Q_EXPORT_PLUGIN(MainWindowPluginImpl)
