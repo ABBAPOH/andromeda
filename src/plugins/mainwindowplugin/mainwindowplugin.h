62011816bcf150064215039f2637fd9e2aac260a
ABBAPOH <ABBAPOH@nextmail.ru>
2011-06-08 21:35:57 +0400

    Added some actions:)

--- src/plugins/mainwindowplugin/mainwindowplugin.h
+++ src/plugins/mainwindowplugin/mainwindowplugin.h
@@ -5,6 +5,16 @@
 
 namespace MainWindowPlugin {
 
+const char * const MENU_FILE = "MENU_FILE";
+const char * const MENU_EDIT = "MENU_EDIT";
+
+const char * const ACTION_NEW_WINDOW = "ACTION_NEW_WINDOW";
+const char * const ACTION_UNDO = "ACTION_UNDO";
+const char * const ACTION_REDO = "ACTION_REDO";
+const char * const ACTION_CUT = "ACTION_CUT";
+const char * const ACTION_COPY = "ACTION_COPY";
+const char * const ACTION_PASTE = "ACTION_PASTE";
+
 class MainWindow;
 
 class MainWindowPluginImpl : public ExtensionSystem::IPlugin
@@ -16,12 +26,13 @@ public:
     virtual bool initialize();
     virtual void shutdown();
 
-    MainWindow *window;
-
 signals:
 
 public slots:
+    void newWindow();
 
+private:
+    void createActions();
 };
 
 }
