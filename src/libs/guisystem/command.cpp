c07e4d31a506a34970d82e0cee8990351a1a7e88
Ivan Komissarov <ABBAPOH@me.com>
2011-06-29 22:45:21 +0400

    Added Command class

---
+++ src/libs/guisystem/command.cpp
@@ -0,0 +1,131 @@
+#include "command.h"
+
+#include <QtGui/QAction>
+#include <QtGui/QKeySequence>
+
+#include "actionmanager.h"
+
+namespace GuiSystem {
+
+class CommandPrivate
+{
+public:
+    QAction *action;
+    QAction *realAction;
+
+    Command::Attributes attributes;
+
+    QKeySequence defaultShortcut;
+    QIcon defaultIcon;
+    QString defaultText;
+
+    QString id;
+};
+
+} // namespace GuiSystem
+
+using namespace GuiSystem;
+
+Command::Command(const QString &id, QObject *parent) :
+    QObject(parent),
+    d_ptr(new CommandPrivate)
+{
+    Q_D(Command);
+
+    d->id = id;
+    d->action = new QAction(this);
+    d->realAction = 0;
+
+    connect(d->action, SIGNAL(triggered(bool)), SLOT(onTrigger(bool)));
+
+//    ActionManager::instance()->registerCommand(this);
+}
+
+Command::~Command()
+{
+    delete d_ptr;
+}
+
+QAction * Command::action() const
+{
+    return d_func()->action;
+}
+
+Command::Attributes Command::attributes() const
+{
+    return d_func()->attributes;
+}
+
+void Command::setAttributes(Attributes attr)
+{
+    d_func()->attributes = attr;
+}
+
+QKeySequence Command::defaultShortcut() const
+{
+    return d_func()->defaultShortcut;
+}
+
+void Command::setDefaultShortcut(const QKeySequence &key)
+{
+    d_func()->defaultShortcut = key;
+}
+
+QIcon Command::defaultIcon() const
+{
+    return d_func()->defaultIcon;
+}
+
+void Command::setDefaultIcon(const QIcon &icon)
+{
+    d_func()->defaultIcon = icon;
+}
+
+QString Command::defaultText() const
+{
+    return d_func()->defaultText;
+}
+
+void Command::setDefaultText(const QString &text)
+{
+    d_func()->defaultText = text;
+}
+
+QString Command::id() const
+{
+    return d_func()->id;
+}
+
+void Command::onTrigger(bool checked)
+{
+    Q_D(Command);
+
+    if (d->realAction) {
+        if (d->realAction->isCheckable())
+            d->realAction->setChecked(checked);
+        else
+            d->realAction->trigger();
+    }
+}
+
+void Command::setRealAction(QAction *action)
+{
+    Q_D(Command);
+
+    d->realAction = action;
+    if (d->attributes & AttributeHide)
+        d->action->setVisible((bool)action);
+    if (d->attributes & AttributeUpdateText)
+        d->action->setText(action ? action->text() : d->defaultText);
+    if (d->attributes & AttributeUpdateIcon)
+        d->action->setIcon(action ? action->icon() : d->defaultIcon);
+    if (!(d->attributes & AttributeNonConfigurable)) {
+        d->action->setShortcut(action ? action->shortcut() : d->defaultShortcut);
+        bool checkable = action->isCheckable();
+        d->action->setCheckable(checkable);
+        if (checkable)
+            d->action->setChecked(action->isChecked());
+    }
+
+    d->action->setEnabled(action->isEnabled());
+}
---
+++ src/libs/guisystem/command.h
@@ -0,0 +1,59 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <QtCore/QObject>
+
+class QAction;
+class QIcon;
+class QKeySequence;
+
+namespace GuiSystem {
+
+class CommandPrivate;
+class Command : public QObject
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(Command)
+public:
+    explicit Command(const QString &id, QObject *parent = 0);
+    ~Command();
+
+    enum Attribute {
+        AttributeHide = 1,
+        AttributeUpdateText = 2,
+        AttributeUpdateIcon = 4,
+        AttributeNonConfigurable = 8
+    };
+    Q_DECLARE_FLAGS(Attributes, Attribute)
+
+    QAction *action() const;
+
+    Attributes attributes() const;
+    void setAttributes(Attributes attr);
+
+    QKeySequence defaultShortcut() const;
+    void setDefaultShortcut(const QKeySequence &key);
+
+    QIcon defaultIcon() const;
+    void setDefaultIcon(const QIcon &icon);
+
+    QString defaultText() const;
+    void setDefaultText(const QString &text);
+
+    QString id() const;
+
+signals:
+
+private slots:
+    void onTrigger(bool);
+
+protected:
+    void setRealAction(QAction *action);
+
+protected:
+    CommandPrivate *d_ptr;
+};
+
+} // namespace GuiSystem
+
+#endif // COMMAND_H
