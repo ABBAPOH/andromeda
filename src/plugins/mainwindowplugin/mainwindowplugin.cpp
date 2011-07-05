70cb591d7204b2648dab041ac39d70d6900dd208
Ivan Komissarov <ABBAPOH@me.com>
2011-07-05 18:32:43 +0400

    Added most common commands

--- src/plugins/mainwindowplugin/mainwindowplugin.cpp
+++ src/plugins/mainwindowplugin/mainwindowplugin.cpp
@@ -46,6 +46,7 @@ void MainWindowPluginImpl::createActions()
 
     CommandContainer *fileContainer = new CommandContainer(MENU_FILE, this);
     fileContainer->setTitle(tr("File"));
+    menuBarContainer->addContainer(fileContainer);
 
     Command *newWindowCommand = new Command(ACTION_NEW_WINDOW, this);
     newWindowCommand->setDefaultText(tr("New window"));
@@ -59,6 +60,16 @@ void MainWindowPluginImpl::createActions()
     newTabCommand->setDefaultShortcut(tr("Ctrl+T"));
     fileContainer->addCommand(newTabCommand);
 
+    Command *newFolderCommand = new Command(ACTION_NEW_FOLDER, this);
+    newFolderCommand->setDefaultText(tr("New folder"));
+    newFolderCommand->setDefaultShortcut(tr("Ctrl+Shift+N"));
+    fileContainer->addCommand(newFolderCommand);
+
+    Command *openCommand = new Command(ACTION_OPEN, this);
+    openCommand->setDefaultText(tr("Open"));
+    openCommand->setDefaultShortcut(tr("Ctrl+O"));
+    fileContainer->addCommand(openCommand);
+
     Command *closeTabCommand = new Command(ACTION_CLOSE_TAB, this);
     closeTabCommand->setDefaultText(tr("Close"));
     closeTabCommand->setDefaultShortcut(tr("Ctrl+W"));
@@ -66,8 +77,6 @@ void MainWindowPluginImpl::createActions()
 
     CommandContainer *editContainer = new CommandContainer(MENU_EDIT, this);
     editContainer->setTitle(tr("Edit"));
-
-    menuBarContainer->addContainer(fileContainer);
     menuBarContainer->addContainer(editContainer);
 
     Command *undoCommand = new Command(ACTION_UNDO, this);
@@ -94,6 +103,39 @@ void MainWindowPluginImpl::createActions()
     pasteCommand->setDefaultText(tr("Paste"));
     pasteCommand->setDefaultShortcut(tr("Ctrl+V"));
     editContainer->addCommand(pasteCommand);
+
+    Command *selectAllCommand = new Command(ACTION_SELECT_ALL, this);
+    selectAllCommand->setDefaultText(tr("Select All"));
+    selectAllCommand->setDefaultShortcut(tr("Ctrl+A"));
+    editContainer->addCommand(selectAllCommand);
+
+    CommandContainer *viewContainer = new CommandContainer(MENU_EDIT, this);
+    viewContainer->setTitle(tr("View"));
+    menuBarContainer->addContainer(viewContainer);
+
+    Command *dualPaneCommand = new Command(ACTION_SELECT_ALL, this);
+    dualPaneCommand->setDefaultText(tr("Dual Pane"));
+    dualPaneCommand->setDefaultShortcut(tr("Ctrl+D"));
+    viewContainer->addCommand(dualPaneCommand);
+
+    CommandContainer *goToContainer = new CommandContainer(MENU_EDIT, this);
+    goToContainer->setTitle(tr("Go To"));
+    menuBarContainer->addContainer(goToContainer);
+
+    Command *backCommand = new Command(ACTION_SELECT_ALL, this);
+    backCommand->setDefaultText(tr("Back"));
+    backCommand->setDefaultShortcut(tr("Ctrl+["));
+    goToContainer->addCommand(backCommand);
+
+    Command *forwardCommand = new Command(ACTION_SELECT_ALL, this);
+    forwardCommand->setDefaultText(tr("Forward"));
+    forwardCommand->setDefaultShortcut(tr("Ctrl+]"));
+    goToContainer->addCommand(forwardCommand);
+
+    Command *upOneLevelCommand = new Command(ACTION_SELECT_ALL, this);
+    upOneLevelCommand->setDefaultText(tr("Up one level"));
+    upOneLevelCommand->setDefaultShortcut(tr("Ctrl+Up"));
+    goToContainer->addCommand(upOneLevelCommand);
 }
 
 Q_EXPORT_PLUGIN(MainWindowPluginImpl)
