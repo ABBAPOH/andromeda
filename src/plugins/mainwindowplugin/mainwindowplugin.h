09d3b107432e2d6794b28b529c2396aa03a58a18
ABBAPOH <ABBAPOH@nextmail.ru>
2011-07-05 18:32:43 +0400

    Added most common commands

--- src/plugins/mainwindowplugin/mainwindowplugin.h
+++ src/plugins/mainwindowplugin/mainwindowplugin.h
@@ -9,15 +9,23 @@ const char * const MENU_BAR = "MENU_BAR";
 
 const char * const MENU_FILE = "MENU_FILE";
 const char * const MENU_EDIT = "MENU_EDIT";
+const char * const MENU_VIEW = "MENU_VIEW";
+const char * const MENU_GOTO = "MENU_GOTO";
 
 const char * const ACTION_NEW_WINDOW = "ACTION_NEW_WINDOW";
 const char * const ACTION_NEW_TAB = "ACTION_NEW_TAB";
+const char * const ACTION_NEW_FOLDER = "ACTION_NEW_FOLDER";
+const char * const ACTION_OPEN = "ACTION_OPEN";
 const char * const ACTION_CLOSE_TAB = "ACTION_CLOSE_TAB";
 const char * const ACTION_UNDO = "ACTION_UNDO";
 const char * const ACTION_REDO = "ACTION_REDO";
 const char * const ACTION_CUT = "ACTION_CUT";
 const char * const ACTION_COPY = "ACTION_COPY";
 const char * const ACTION_PASTE = "ACTION_PASTE";
+const char * const ACTION_SELECT_ALL = "ACTION_SELECT_ALL";
+const char * const ACTION_BACK = "ACTION_BACK";
+const char * const ACTION_FORWARD = "ACTION_FORWARD";
+const char * const ACTION_UP = "ACTION_UP";
 
 class MainWindow;
 
