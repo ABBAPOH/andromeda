53308aeb7c8c6617ab3e958bcf9ba209a26d9414
Ivan Komissarov <ABBAPOH@me.com>
2011-07-18 22:07:32 +0400

    Fixed crash in debug:)

--- src/libs/io/qdrivecontroller_win.cpp
+++ src/libs/io/qdrivecontroller_win.cpp
@@ -142,6 +142,7 @@ static inline HWND dw_create_internal_window(const void* userData)
 {
     HINSTANCE hi = qWinAppInst();
 
+    QString className = ::className(); // not to destroy temporary variable
     WNDCLASS wc;
     wc.style = 0;
     wc.lpfnWndProc = dw_internal_proc;
@@ -152,7 +153,7 @@ static inline HWND dw_create_internal_window(const void* userData)
     wc.hCursor = 0;
     wc.hbrBackground = 0;
     wc.lpszMenuName = NULL;
-    wc.lpszClassName = reinterpret_cast<const wchar_t *>(className().utf16());
+    wc.lpszClassName = reinterpret_cast<const wchar_t *>(className.utf16());
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindow(wc.lpszClassName,       // classname
