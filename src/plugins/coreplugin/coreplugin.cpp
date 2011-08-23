e9b755ec8d86225bb34a75aca4091c3cc55196e4
Ivan Komissarov <ABBAPOH@me.com>
2011-08-23 18:51:45 +0400

    GoTo locations moved to FilemanagerPlugin

--- src/plugins/coreplugin/coreplugin.cpp
+++ src/plugins/coreplugin/coreplugin.cpp
@@ -3,9 +3,7 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QTimer>
 #include <QtCore/QtPlugin>
-#include <QtCore/QDir>
 #include <QtGui/QMenu>
-#include <QtGui/QDesktopServices>
 
 #include "constants.h"
 #include "core.h"
@@ -243,21 +241,6 @@ void CorePluginImpl::createActions()
     upOneLevelCommand->setDefaultIcon(QIcon(":/images/icons/up.png"));
     upOneLevelCommand->setDefaultShortcut(tr("Ctrl+Up"));
     goToContainer->addCommand(upOneLevelCommand);
-    
-    // ================ GoTo Menu (Locations) ================
-    goToContainer->addGroup(Constants::Ids::MenuGroups::Locations);
-
-    createGotoDirCommand(QDesktopServices::DesktopLocation, QIcon(":/images/icons/desktopFolder.png"), tr("Ctrl+Shift+D"));
-    createGotoDirCommand(QDesktopServices::HomeLocation,
-                         QIcon::fromTheme("go-home", QIcon(":/images/icons/homeFolder.png")),
-                         tr("Ctrl+Shift+H"));
-    createGotoDirCommand(QDesktopServices::DocumentsLocation, QIcon(":/images/icons/documentsFolder.png"), tr("Ctrl+Shift+O"));
-#ifdef Q_OS_MAC
-    createGotoDirCommand(QDesktopServices::ApplicationsLocation, QIcon(":/images/icons/appsFolder.png"), tr("Ctrl+Shift+A"));
-#endif
-    createGotoDirCommand(QDesktopServices::MusicLocation, QIcon(":/images/icons/musicFolder.png"));
-    createGotoDirCommand(QDesktopServices::MoviesLocation, QIcon(":/images/icons/movieFolder.png"));
-    createGotoDirCommand(QDesktopServices::PicturesLocation, QIcon(":/images/icons/picturesFolder.png"));
 
     // ================ Tools Menu ================
     CommandContainer *toolsContainer = new CommandContainer(Constants::Ids::Menus::Tools, this);
@@ -270,39 +253,6 @@ void CorePluginImpl::createActions()
     pluginsCommand->setContext(Command::ApplicationCommand);
     connect(pluginsCommand->action(), SIGNAL(triggered()), SLOT(showPluginView()));
     toolsContainer->addCommand(pluginsCommand);
-}
-
-void CorePluginImpl::createGotoDirCommand(QDesktopServices::StandardLocation location, const QIcon &icon, const QKeySequence &key)
-{
-    GuiSystem::CommandContainer * container = ActionManager::instance()->container(Constants::Ids::Menus::GoTo);
-    QDir dir(QDesktopServices::storageLocation(location));
-
-    if (!dir.exists())
-        return;
-
-    QString id = QString(QLatin1String(Constants::Ids::Actions::Goto)).arg(location);
-    Command *cmd = new Command(id.toLatin1(), this);
-    cmd->setData(dir.absolutePath());
-    QString displayName(QDesktopServices::displayName(location));
-    // fir for broken linux Qt
-    if (displayName.isEmpty())
-    {
-        QString path = dir.absolutePath();
-        int index = path.lastIndexOf(QLatin1Char('/'));
-        if (index != -1)
-            displayName = path.mid(index + 1);
-    }
-
-    cmd->setDefaultText(displayName);
-    cmd->setDefaultShortcut(key);
-
-    if (!icon.isNull())
-        cmd->setDefaultIcon(icon);
-
-    cmd->setData(dir.absolutePath());
-    cmd->setContext(Command::ApplicationCommand);
-
-    container->addCommand(cmd, Constants::Ids::MenuGroups::Locations);
 }
 
 Q_EXPORT_PLUGIN(CorePluginImpl)
--- src/plugins/filemanagerplugin/filemanagerpluginimpl.cpp
+++ src/plugins/filemanagerplugin/filemanagerpluginimpl.cpp
@@ -1,6 +1,11 @@
 #include "filemanagerpluginimpl.h"
 
 #include <QtCore/QtPlugin>
+#include <QtCore/QDir>
+
+#include <actionmanager.h>
+#include <command.h>
+#include <commandcontainer.h>
 #include <perspective.h>
 #include <guicontroller.h>
 #include <CorePlugin>
@@ -36,7 +41,56 @@ bool FileManagerPluginImpl::initialize()
     addObject(new FileCopyDialog, "fileCopyDialog");
     addObject(FileSystemManager::instance());
 
+    CommandContainer *goToContainer = ActionManager::instance()->container(Constants::Ids::Menus::GoTo);
+    // ================ GoTo Menu (Locations) ================
+    goToContainer->addGroup(Constants::Ids::MenuGroups::Locations);
+
+    createGotoDirCommand(QDesktopServices::DesktopLocation, QIcon(":/images/icons/desktopFolder.png"), tr("Ctrl+Shift+D"));
+    createGotoDirCommand(QDesktopServices::HomeLocation,
+                         QIcon::fromTheme("go-home", QIcon(":/images/icons/homeFolder.png")),
+                         tr("Ctrl+Shift+H"));
+    createGotoDirCommand(QDesktopServices::DocumentsLocation, QIcon(":/images/icons/documentsFolder.png"), tr("Ctrl+Shift+O"));
+#ifdef Q_OS_MAC
+    createGotoDirCommand(QDesktopServices::ApplicationsLocation, QIcon(":/images/icons/appsFolder.png"), tr("Ctrl+Shift+A"));
+#endif
+    createGotoDirCommand(QDesktopServices::MusicLocation, QIcon(":/images/icons/musicFolder.png"));
+    createGotoDirCommand(QDesktopServices::MoviesLocation, QIcon(":/images/icons/movieFolder.png"));
+    createGotoDirCommand(QDesktopServices::PicturesLocation, QIcon(":/images/icons/picturesFolder.png"));
+
     return true;
+}
+
+void FileManagerPluginImpl::createGotoDirCommand(QDesktopServices::StandardLocation location, const QIcon &icon, const QKeySequence &key)
+{
+    GuiSystem::CommandContainer * container = ActionManager::instance()->container(Constants::Ids::Menus::GoTo);
+    QDir dir(QDesktopServices::storageLocation(location));
+
+    if (!dir.exists())
+        return;
+
+    QString id = QString(QLatin1String(Constants::Ids::Actions::Goto)).arg(location);
+    Command *cmd = new Command(id.toLatin1(), this);
+    cmd->setData(dir.absolutePath());
+    QString displayName(QDesktopServices::displayName(location));
+    // fir for broken linux Qt
+    if (displayName.isEmpty())
+    {
+        QString path = dir.absolutePath();
+        int index = path.lastIndexOf(QLatin1Char('/'));
+        if (index != -1)
+            displayName = path.mid(index + 1);
+    }
+
+    cmd->setDefaultText(displayName);
+    cmd->setDefaultShortcut(key);
+
+    if (!icon.isNull())
+        cmd->setDefaultIcon(icon);
+
+    cmd->setData(dir.absolutePath());
+    cmd->setContext(Command::ApplicationCommand);
+
+    container->addCommand(cmd, Constants::Ids::MenuGroups::Locations);
 }
 
 Q_EXPORT_PLUGIN(FileManagerPluginImpl)
