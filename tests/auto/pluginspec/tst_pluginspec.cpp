aef475965f902983beb6115409e8b83ef5649283
Ivan Komissarov <ABBAPOH@me.com>
2011-08-23 21:06:41 +0400

    Finished pluginspec IO test

--- tests/auto/pluginspec/tst_pluginspec.cpp
+++ tests/auto/pluginspec/tst_pluginspec.cpp
@@ -1,6 +1,11 @@
 #include <QtCore/QString>
 #include <QtTest/QtTest>
 
+#include <pluginspec.h>
+#include <mutablepluginspec.h>
+
+using namespace ExtensionSystem;
+
 class PluginSpecTest : public QObject
 {
     Q_OBJECT
@@ -12,7 +17,9 @@ private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void testSerialise();
-    void testSerialise_data();
+
+private:
+    void compareSpecs(PluginSpec *first, PluginSpec *second);
 };
 
 PluginSpecTest::PluginSpecTest()
@@ -29,19 +36,38 @@ void PluginSpecTest::cleanupTestCase()
 
 void PluginSpecTest::testSerialise()
 {
-    qDebug() << "PluginSpecTest::testSerialise";
-    QFETCH(QString, data);
-    QFETCH(QString, data2);
-    qDebug() << data << data2;
-//    QVERIFY2(true, "Failure");
+    MutablePluginSpec spec;
+    spec.setName(QLatin1String("Name"));
+    spec.setVersion(Version::fromString(QLatin1String("1.2.3.4")));
+    spec.setCompatibilityVersion(Version::fromString(QLatin1String("1.2.3.4")));
+    spec.setVendor(QLatin1String("Vendor"));
+    spec.setCategory(QLatin1String("Category"));
+    spec.setCopyright(QLatin1String("Copyright"));
+    spec.setLicense(QLatin1String("License"));
+    spec.setDescription(QLatin1String("Description"));
+    spec.setUrl(QLatin1String("Url"));
+    spec.setDependencies(QList<PluginDependency>() << PluginDependency(QLatin1String("Name"), QLatin1String("1.2.3.4")));
+
+    spec.write(QLatin1String("File.pluginspec"));
+
+    PluginSpec spec2;
+    spec2.read(QLatin1String("File.pluginspec"));
+
+    compareSpecs(&spec, &spec2);
 }
 
-void PluginSpecTest::testSerialise_data()
+void PluginSpecTest::compareSpecs(PluginSpec *first, PluginSpec *second)
 {
-    QTest::addColumn<QString>("data");
-    QTest::addColumn<QString>("data2");
-    QTest::newRow("0") << QString("hello world") << "blas";
-    QTest::newRow("1") << QString("hello world") << "lol";
+    QCOMPARE(first->name(), second->name());
+    QCOMPARE(first->version(), second->version());
+    QCOMPARE(first->compatibilityVersion(), second->compatibilityVersion());
+    QCOMPARE(first->vendor(), second->vendor());
+    QCOMPARE(first->category(), second->category());
+    QCOMPARE(first->copyright(), second->copyright());
+    QCOMPARE(first->license(), second->license());
+    QCOMPARE(first->description(), second->description());
+    QCOMPARE(first->url(), second->url());
+    QCOMPARE(first->dependencies(), second->dependencies());
 }
 
 QTEST_APPLESS_MAIN(PluginSpecTest)
