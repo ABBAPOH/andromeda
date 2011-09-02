037e93d8c0be2b7b8e644e7672254d2ffea49339
Ivan Komissarov <ABBAPOH@me.com>
2011-09-02 20:33:10 +0400

    Changed lib suffix a little. Hope will work:)

--- LibSuffix.cmake
+++ LibSuffix.cmake
@@ -1,20 +1,21 @@
 # some system (rpm builds) setup LIB_SUFFIX for cmake. If there is no set, try to get it from system
-IF (NOT LIB_SUFFIX) 
-        IF (NOT APPLE) 
-                # check 64 bit 
-                IF (CMAKE_SIZEOF_VOID_P EQUAL 4) 
-                        SET (LIB_SUFFIX "") 
-                ELSE (CMAKE_SIZEOF_VOID_P EQUAL 4) 
-                        SET (LIB_SUFFIX "64") 
-                        MESSAGE(STATUS " LIB_SUFFIX is set to '${LIB_SUFFIX}'") 
-                ENDIF (CMAKE_SIZEOF_VOID_P EQUAL 4) 
-        ELSE (NOT APPLE) 
-                SET (LIB_SUFFIX "") 
-        ENDIF (NOT APPLE)
+if( NOT LIB_SUFFIX )
+
+    if( UNIX AND NOT APPLE)
         # BSD does not use lib64 for 64bit libs
-        IF (${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
-                message(STATUS "LIB_SUFFIX is disabled for *BSD: ${CMAKE_SYSTEM_NAME}")
-                SET (LIB_SUFFIX "")
-        ENDIF()
-        #
-ENDIF (NOT LIB_SUFFIX)
+        if( ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD" )
+#            message(STATUS "LIB_SUFFIX is disabled for *BSD: ${CMAKE_SYSTEM_NAME}")
+            set( LIB_SUFFIX "" )
+        else()
+            # check 64 bit
+            if( CMAKE_SIZEOF_VOID_P EQUAL 4 )
+                set( LIB_SUFFIX "" )
+            else()
+                set( LIB_SUFFIX "64" )
+            endif()
+        endif()
+    else()
+        set( LIB_SUFFIX "" )
+    endif()
+
+endif()
