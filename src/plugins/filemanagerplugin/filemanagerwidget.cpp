4934ebb911b1f7e9857c9bebcd999167f0d5e4df
Ivan Komissarov <ABBAPOH@me.com>
2011-09-21 00:23:25 +0400

    Fixed 'Return' bug

--- src/plugins/filemanagerplugin/filemanagerwidget.cpp
+++ src/plugins/filemanagerplugin/filemanagerwidget.cpp
@@ -61,7 +61,7 @@ void FileManagerWidgetPrivate::initViews()
         views[i]->setSelectionBehavior(QAbstractItemView::SelectRows);
         views[i]->setDragDropMode(QAbstractItemView::DragDrop);
         views[i]->setAcceptDrops(true);
-        views[i]->setEditTriggers(QAbstractItemView::SelectedClicked);
+        views[i]->setEditTriggers(QAbstractItemView::SelectedClicked | QAbstractItemView::EditKeyPressed);
 //        QAbstractItemView::InternalMove
         layout->addWidget(views[i]);
 //        views[i]->setDragEnabled(true);
