4f41e63c6ee7c18fbac82bf805417bbbd159ad67
ABBAPOH <ABBAPOH@nextmail.ru>
2011-10-06 22:19:52 +0400

    Added error handling for reading/writing specs.

--- src/libs/extensionsystem/pluginspecformathandler_p.h
+++ src/libs/extensionsystem/pluginspecformathandler_p.h
@@ -10,13 +10,22 @@ namespace ExtensionSystem {
 class PluginSpecFormatHandler
 {
 public:
-    PluginSpecFormatHandler() {}
+    PluginSpecFormatHandler() { clearError(); }
     virtual ~PluginSpecFormatHandler() {}
 
     virtual bool canRead(QIODevice *device) = 0;
 
     virtual bool read(QIODevice *device, PluginSpecPrivate *d) = 0;
     virtual bool write(QIODevice *device, PluginSpecPrivate *d) = 0;
+
+    QString errorString() const { return m_errorString; }
+
+protected:
+    inline void setErrorString(const QString &errorString) { m_errorString = errorString; }
+    inline void clearError() { m_errorString = QObject::tr("No error"); }
+
+private:
+    QString m_errorString;
 };
 
 class PluginSpecBinaryHandler : public PluginSpecFormatHandler
