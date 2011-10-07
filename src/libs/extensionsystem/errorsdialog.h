766451c63125c82f674257cc90a331d8c3518f79
ABBAPOH <ABBAPOH@nextmail.ru>
2011-10-07 16:09:40 +0400

    Added ExtensionSystem::ErrorsDialog

---
+++ src/libs/extensionsystem/errorsdialog.h
@@ -0,0 +1,33 @@
+#ifndef ERRORSDIALOG_H
+#define ERRORSDIALOG_H
+
+#include "extensionsystem_global.h"
+
+#include <QtGui/QDialog>
+
+class QLabel;
+class QListView;
+class QStringListModel;
+class QDialogButtonBox;
+
+class EXTENSIONSYSTEM_EXPORT ErrorsDialog : public QDialog
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(ErrorsDialog)
+
+public:
+    explicit ErrorsDialog(QWidget *parent = 0);
+
+    QString message() const;
+    void setMessage(const QString &message);
+
+    void setErrors(const QStringList &errors);
+
+private:
+    QLabel *m_label;
+    QListView *m_view;
+    QStringListModel *m_model;
+    QDialogButtonBox *m_buttonBox;
+};
+
+#endif // ERRORSDIALOG_H
