25c51ec0fff9bbeed5bf5cb7c80974ed83b2012d
Ivan Komissarov <ABBAPOH@me.com>
2011-10-14 19:22:49 +0400

    Fixes to build on win

--- ProjectInfo.cmake
+++ ProjectInfo.cmake
@@ -1,9 +1,15 @@
+find_package( git )
+
 macro( get_git_revision VAR)
+if( GIT_EXECUTABLE )
 exec_program(
-    "git"
+    ${GIT_EXECUTABLE}
     ${PROJECT_SOURCE_DIR}
     ARGS "rev-parse -q HEAD"
     OUTPUT_VARIABLE ${VAR} )
+else()
+    set( ${VAR} "unknown" )
+endif()
 endmacro( get_git_revision )
 
 get_git_revision(GIT_REVISION)
