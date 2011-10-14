0b7762c51a90e2410334bbbde16c176b1ec7f80a
ABBAPOH <ABBAPOH@nextmail.ru>
2011-10-14 16:55:42 +0400

    Added ru translations for FileManagerPlugin

---
+++ src/plugins/filemanagerplugin/translations/filemanagerplugin_ru.ts
@@ -0,0 +1,510 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>FileCopyErrorDialog</name>
+    <message>
+        <location filename="../filecopyerrordialog.ui" line="14"/>
+        <source>Copying</source>
+        <translation>Копирование</translation>
+    </message>
+</context>
+<context>
+    <name>FileCopyReplaceDialog</name>
+    <message>
+        <location filename="../filecopyreplacedialog.ui" line="14"/>
+        <source>Copying</source>
+        <translation>Копирование</translation>
+    </message>
+    <message>
+        <location filename="../filecopyreplacedialog.ui" line="49"/>
+        <source>Apply to All</source>
+        <translation>Применить ко всем</translation>
+    </message>
+    <message>
+        <location filename="../filecopyreplacedialog.ui" line="81"/>
+        <source>Skip</source>
+        <translation>Пропустить</translation>
+    </message>
+    <message>
+        <location filename="../filecopyreplacedialog.ui" line="103"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../filecopyreplacedialog.ui" line="119"/>
+        <source>Overwrite</source>
+        <translation>Перезаписать</translation>
+    </message>
+</context>
+<context>
+    <name>FileCopyWidget</name>
+    <message>
+        <location filename="../filecopywidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="27"/>
+        <source>&lt;name here&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="46"/>
+        <location filename="../filecopywidget.ui" line="78"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="113"/>
+        <source>Bytes copied:</source>
+        <translation>Скопировано байт:</translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="120"/>
+        <location filename="../filecopywidget.ui" line="154"/>
+        <source>&lt;copied&gt; / &lt;total&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="130"/>
+        <source>Bytes per second:</source>
+        <translation>Байт в секунду:</translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="137"/>
+        <source>&lt;speed&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="147"/>
+        <source>Objects copied:</source>
+        <translation>Объектов скопировано:</translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="164"/>
+        <source>Time left:</source>
+        <translation>Времени осталось:</translation>
+    </message>
+    <message>
+        <location filename="../filecopywidget.ui" line="171"/>
+        <source>&lt;remains&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileInfoDialog</name>
+    <message>
+        <location filename="../fileinfodialog.ui" line="20"/>
+        <source>File info</source>
+        <translation>Информация о файле</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="71"/>
+        <source>&lt;name&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="96"/>
+        <source>Mime Type:</source>
+        <translation>Тип mime:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="103"/>
+        <source>&lt;mime&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="110"/>
+        <source>Size:</source>
+        <translation>Размер:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="117"/>
+        <source>&lt;size&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="124"/>
+        <source>Location:</source>
+        <translation>Расположение:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="131"/>
+        <source>&lt;location&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="141"/>
+        <source>Created:</source>
+        <translation>Создан:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="148"/>
+        <source>&lt;created&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="155"/>
+        <source>Modified:</source>
+        <translation>Изменён:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="162"/>
+        <source>&lt;modified&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="169"/>
+        <source>Last access:</source>
+        <translation>Последний доступ:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="176"/>
+        <source>&lt;accessed&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="211"/>
+        <source>User:</source>
+        <translation>Пользователь:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="219"/>
+        <location filename="../fileinfodialog.ui" line="240"/>
+        <location filename="../fileinfodialog.ui" line="261"/>
+        <source>Read only</source>
+        <translation>Только чтение</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="224"/>
+        <location filename="../fileinfodialog.ui" line="245"/>
+        <location filename="../fileinfodialog.ui" line="266"/>
+        <source>Read write</source>
+        <translation>Чтение и запись</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="232"/>
+        <source>Group:</source>
+        <translation>Группа:</translation>
+    </message>
+    <message>
+        <location filename="../fileinfodialog.ui" line="253"/>
+        <source>Other:</source>
+        <translation>Другие:</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPlugin::FileCopyDialogPrivate</name>
+    <message>
+        <location filename="../filecopydialog.cpp" line="98"/>
+        <source>%1 &quot;%2&quot; is already exists. Would you like to replace it?</source>
+        <translation>%1 &quot;%2&quot; уже существует. Заменить?</translation>
+    </message>
+    <message>
+        <location filename="../filecopydialog.cpp" line="99"/>
+        <location filename="../filecopydialog.cpp" line="121"/>
+        <source>Folder</source>
+        <translation>Папка</translation>
+    </message>
+    <message>
+        <location filename="../filecopydialog.cpp" line="99"/>
+        <location filename="../filecopydialog.cpp" line="121"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../filecopydialog.cpp" line="120"/>
+        <source>Error occured for %1 &quot;%2&quot;: %3</source>
+        <translation>Произошла ошибка для %1 &quot;%2&quot;: %3</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPlugin::FileManagerEditor</name>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="212"/>
+        <source>View Mode</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="307"/>
+        <source>Cut &quot;%1&quot;</source>
+        <translation>Вырезать &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="308"/>
+        <source>Copy &quot;%1&quot;</source>
+        <translation>Копировать &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="310"/>
+        <source>Cut %1 items</source>
+        <translation>Вырезать %1 элементов</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="311"/>
+        <source>Copy %1 items</source>
+        <translation>Копировать %1 элементов</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="314"/>
+        <location filename="../filemanagereditor.cpp" line="315"/>
+        <location filename="../filemanagereditor.cpp" line="444"/>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="426"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="427"/>
+        <source>New Folder</source>
+        <translation>Новая папка</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="428"/>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="429"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="431"/>
+        <source>File info</source>
+        <translation>Информация о файле</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="436"/>
+        <source>Redo</source>
+        <translation>Повторить</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="437"/>
+        <source>Undo</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="443"/>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="445"/>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="446"/>
+        <source>Select all</source>
+        <translation>Выделить все</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="450"/>
+        <source>Show hidden files</source>
+        <translation>Показывать скрытые файлы</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="453"/>
+        <source>Show left panel</source>
+        <translation>Показывать левую панель</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="480"/>
+        <source>Icon view</source>
+        <translation>Иконки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="481"/>
+        <source>Column view</source>
+        <translation>Колонки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="482"/>
+        <source>Tree view</source>
+        <translation>Дерево</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="483"/>
+        <source>Cover flow</source>
+        <translation>Cover flow</translation>
+    </message>
+    <message>
+        <location filename="../filemanagereditor.cpp" line="484"/>
+        <source>Dual pane</source>
+        <translation>Две панели</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPlugin::FileManagerSettingsPage</name>
+    <message>
+        <location filename="../filemanagersettingspage.cpp" line="17"/>
+        <source>Global</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingspage.cpp" line="22"/>
+        <source>File Manager</source>
+        <translation>Файловый менеджер</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPlugin::FileManagerWidget</name>
+    <message>
+        <location filename="../filemanagerwidget.cpp" line="299"/>
+        <source>New Folder</source>
+        <translation>Новая папка</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPlugin::NavigationPanel</name>
+    <message>
+        <location filename="../navigationpanel.cpp" line="66"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../navigationpanel.cpp" line="68"/>
+        <source>Remove from panel</source>
+        <translation>Убрать с панели</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPluginImpl</name>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="100"/>
+        <source>File info</source>
+        <translation>Информация о файле</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="108"/>
+        <source>New folder</source>
+        <translation>Новая папка</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="113"/>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="122"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="143"/>
+        <source>Icon View</source>
+        <translation>Иконки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="149"/>
+        <source>Column View</source>
+        <translation>Колонки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="155"/>
+        <source>Tree View</source>
+        <translation>Дерево</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="161"/>
+        <source>Cover flow</source>
+        <translation>Cover flow</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="167"/>
+        <source>Dual Pane</source>
+        <translation>Две панели</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="173"/>
+        <source>Show Hidden Files</source>
+        <translation>Показывать скрытые файлы</translation>
+    </message>
+    <message>
+        <location filename="../filemanagerplugin.cpp" line="179"/>
+        <source>Show left panel</source>
+        <translation>Показывать левую панель</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerSettingsWidget</name>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="20"/>
+        <source>Left Panel</source>
+        <translation>Левая панель</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="26"/>
+        <source>Folders</source>
+        <translation>Папки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="33"/>
+        <source>Applications</source>
+        <translation>Приложения</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="40"/>
+        <source>Desktop</source>
+        <translation>Рабочий стол</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="47"/>
+        <source>Documents</source>
+        <translation>Документы</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="57"/>
+        <source>Downloads</source>
+        <translation>Загрузки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="64"/>
+        <source>Movies</source>
+        <translation>Фильмы</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="71"/>
+        <source>Music</source>
+        <translation>Музыка</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="78"/>
+        <source>Pictures</source>
+        <translation>Картинки</translation>
+    </message>
+    <message>
+        <location filename="../filemanagersettingswidget.ui" line="85"/>
+        <source>Home</source>
+        <translation>Домашня папка</translation>
+    </message>
+</context>
+<context>
+    <name>NavigationModel</name>
+    <message>
+        <location filename="../navigationmodel.cpp" line="133"/>
+        <source>Devices</source>
+        <translation>Устройства</translation>
+    </message>
+    <message>
+        <location filename="../navigationmodel.cpp" line="134"/>
+        <source>Network</source>
+        <translation>Сеть</translation>
+    </message>
+    <message>
+        <location filename="../navigationmodel.cpp" line="135"/>
+        <source>Folders</source>
+        <translation>Папки</translation>
+    </message>
+</context>
+</TS>
