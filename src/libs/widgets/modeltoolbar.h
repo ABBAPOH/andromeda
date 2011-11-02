9841e43b880c018956afc9c21240293d261e947d
ABBAPOH <ABBAPOH@nextmail.ru>
2011-11-02 14:57:26 +0400

    Added ModelToolBar

---
+++ src/libs/widgets/modeltoolbar.h
@@ -0,0 +1,51 @@
+#ifndef MODELTOOLBAR_H
+#define MODELTOOLBAR_H
+
+#include <QtGui/QToolBar>
+
+class ModelMenu;
+class QAbstractItemModel;
+class QEvent;
+class QModelIndex;
+
+class ModelToolBarPrivate;
+class ModelToolBar : public QToolBar
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(ModelToolBar)
+
+public:
+    explicit ModelToolBar(QWidget *parent = 0);
+    explicit ModelToolBar(const QString &title, QWidget *parent = 0);
+    ~ModelToolBar();
+
+    void setModel(QAbstractItemModel *model);
+    QAbstractItemModel *model() const;
+
+    void setRootIndex(const QModelIndex &index);
+    QModelIndex rootIndex() const;
+
+    static QModelIndex index(QAction *action);
+
+signals:
+    void activated(const QModelIndex &index);
+
+protected:
+    virtual ModelMenu *createMenu();
+
+    bool eventFilter(QObject *object, QEvent *event);
+
+    void hideEvent(QHideEvent *event);
+    void showEvent(QShowEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+protected slots:
+    void build();
+
+private:
+    ModelToolBarPrivate *d;
+};
+
+#endif // MODELTOOLBAR_H
