b1fb3a6603d706ed1ca0ae66afdc46212664fae5
Ivan Komissarov <ABBAPOH@me.com>
2011-11-10 19:37:44 +0400

    Added AddressBar

---
+++ src/libs/widgets/addressbar.cpp
@@ -0,0 +1,119 @@
+#include "addressbar.h"
+
+#include <QtGui/QApplication>
+#include <QtGui/QPaintEvent>
+
+static inline QString urlToUserOutput(const QUrl &url)
+{
+    if (url.scheme() == QLatin1String("file"))
+        return url.toLocalFile();
+    else if (url.scheme() == qApp->applicationName())
+        return QString();
+    else
+        return url.toString();
+}
+
+AddressBar::AddressBar(QWidget *parent) :
+    FancyLineEdit(parent),
+    m_loading(false),
+    m_progress(0)
+{
+    setFrame(false);
+    setButtonVisible(Right, true);
+    setButtonPixmap(Right, QIcon(":/icons/refresh.png").pixmap(16));
+
+    connect(this, SIGNAL(rightButtonClicked()), SLOT(onRightButtonClicked()));
+}
+
+void AddressBar::setLoading(bool yes)
+{
+    m_loading = yes;
+    if (yes)
+        setButtonPixmap(Right, QIcon(":/icons/cancelLoading.png").pixmap(16));
+    else
+        setButtonPixmap(Right, QIcon(":/icons/refresh.png").pixmap(16));
+}
+
+void AddressBar::setUrl(const QUrl &url)
+{
+    m_url = url;
+    setText(urlToUserOutput(url));
+}
+
+void AddressBar::startLoad()
+{
+    setLoading(true);
+}
+
+void AddressBar::finishLoad()
+{
+    setLoading(false);
+    m_progress = 0;
+    update();
+}
+
+void AddressBar::setLoadProgress(int progress)
+{
+    if (!m_loading)
+        setLoading(true);
+    m_progress = progress;
+    update();
+}
+
+void AddressBar::keyPressEvent(QKeyEvent *e)
+{
+    QLineEdit::keyPressEvent(e);
+
+    if (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) {
+        updateUrl();
+    }
+}
+
+void AddressBar::paintEvent(QPaintEvent *event)
+{
+    QPalette p = palette();
+
+    QColor defaultBaseColor = QApplication::palette().color(QPalette::Base);
+    QColor backgroundColor = defaultBaseColor;
+    if (m_url.scheme() == QLatin1String("https")
+        && p.color(QPalette::Text).value() < 128) {
+        QColor lightYellow(248, 248, 210);
+        backgroundColor = lightYellow;
+    }
+
+    // set the progress bar
+    if (m_progress == 0) {
+        p.setBrush(QPalette::Base, backgroundColor);
+    } else {
+        QColor loadingColor = QColor(116, 192, 250);
+        if (p.color(QPalette::Text).value() >= 128)
+            loadingColor = defaultBaseColor.darker(200);
+
+        QLinearGradient gradient(0, 0, width(), 0);
+        gradient.setColorAt(0, loadingColor);
+        gradient.setColorAt(m_progress/100.0, backgroundColor);
+        p.setBrush(QPalette::Base, gradient);
+    }
+    setPalette(p);
+
+    FancyLineEdit::paintEvent(event);
+}
+
+void AddressBar::updateUrl()
+{
+    QUrl url = QUrl::fromUserInput(text());
+    if (m_url != url) {
+        m_url = url;
+        emit open(m_url);
+    } else {
+        emit refresh();
+    }
+}
+
+void AddressBar::onRightButtonClicked()
+{
+    if (m_loading)
+        emit canceled();
+    else
+        updateUrl();
+}
---
+++ src/libs/widgets/addressbar.h
@@ -0,0 +1,43 @@
+#ifndef ADDRESSBAR_H
+#define ADDRESSBAR_H
+
+#include "fancylineedit.h"
+
+#include <QtCore/QUrl>
+
+class WIDGETS_EXPORT AddressBar : public FancyLineEdit
+{
+    Q_OBJECT
+public:
+    explicit AddressBar(QWidget *parent = 0);
+
+    void setLoading(bool);
+
+public slots:
+    void setUrl(const QUrl &url);
+    void startLoad();
+    void finishLoad();
+    void setLoadProgress(int);
+
+signals:
+    void canceled();
+    void refresh();
+    void open(const QUrl &url);
+
+protected:
+    void keyPressEvent(QKeyEvent *);
+    void paintEvent(QPaintEvent *);
+
+private slots:
+    void onRightButtonClicked();
+
+private:
+    void updateUrl();
+
+private:
+    QUrl m_url;
+    bool m_loading;
+    int m_progress;
+};
+
+#endif // ADDRESSBAR_H
