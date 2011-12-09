db9cd9f0fcf18a7af253578c87805f8112a6bd32
ABBAPOH <ABBAPOH@nextmail.ru>
2011-12-10 02:20:05 +0400

    Add ProxySettings

---
+++ src/plugins/webviewplugin/proxysettings.cpp
@@ -0,0 +1,88 @@
+#include "proxysettings.h"
+#include "ui_proxysettings.h"
+
+#include <QtCore/QSettings>
+#include <QtGui/QPushButton>
+
+#include "webviewplugin.h"
+
+ProxySettingsWidget::ProxySettingsWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ProxySettingsWidget),
+    m_settings(new QSettings(this))
+{
+    ui->setupUi(this);
+
+    m_settings->beginGroup(QLatin1String("webview/proxy"));
+
+    loadSettings();
+
+    connect(ui->proxySupport, SIGNAL(toggled(bool)), SLOT(makeDirty()));
+    connect(ui->proxyType, SIGNAL(currentIndexChanged(int)), SLOT(makeDirty()));
+    connect(ui->proxyHostName, SIGNAL(textChanged(QString)), SLOT(makeDirty()));
+    connect(ui->proxyPort, SIGNAL(valueChanged(int)), SLOT(makeDirty()));
+    connect(ui->proxyUserName, SIGNAL(textChanged(QString)), SLOT(makeDirty()));
+    connect(ui->proxyPassword, SIGNAL(textChanged(QString)), SLOT(makeDirty()));
+
+    connect(ui->buttonBox, SIGNAL(clicked(QAbstractButton*)), SLOT(onClick(QAbstractButton*)));
+}
+
+ProxySettingsWidget::~ProxySettingsWidget()
+{
+    delete ui;
+}
+
+void ProxySettingsWidget::onClick(QAbstractButton *button)
+{
+    QDialogButtonBox::ButtonRole role = ui->buttonBox->buttonRole(button);
+
+    if (role == QDialogButtonBox::ApplyRole)
+        saveSettings();
+    else if (role == QDialogButtonBox::ResetRole)
+        loadSettings();
+}
+
+void ProxySettingsWidget::makeDirty()
+{
+    ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(true);
+    ui->buttonBox->button(QDialogButtonBox::Reset)->setEnabled(true);
+}
+
+void ProxySettingsWidget::loadSettings()
+{
+    ui->proxySupport->setChecked(m_settings->value(QLatin1String("enabled"), false).toBool());
+    ui->proxyType->setCurrentIndex(m_settings->value(QLatin1String("type"), 0).toInt());
+    ui->proxyHostName->setText(m_settings->value(QLatin1String("hostName")).toString());
+    ui->proxyPort->setValue(m_settings->value(QLatin1String("port"), 1080).toInt());
+    ui->proxyUserName->setText(m_settings->value(QLatin1String("userName")).toString());
+    ui->proxyPassword->setText(m_settings->value(QLatin1String("password")).toString());
+
+    ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
+    ui->buttonBox->button(QDialogButtonBox::Reset)->setEnabled(false);
+}
+
+void ProxySettingsWidget::saveSettings()
+{
+    m_settings->setValue(QLatin1String("enabled"), ui->proxySupport->isChecked());
+    m_settings->setValue(QLatin1String("type"), ui->proxyType->currentIndex());
+    m_settings->setValue(QLatin1String("hostName"), ui->proxyHostName->text());
+    m_settings->setValue(QLatin1String("port"), ui->proxyPort->text());
+    m_settings->setValue(QLatin1String("userName"), ui->proxyUserName->text());
+    m_settings->setValue(QLatin1String("password"), ui->proxyPassword->text());
+    m_settings->sync();
+
+    ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(false);
+    ui->buttonBox->button(QDialogButtonBox::Reset)->setEnabled(false);
+
+    WebViewPluginImpl::instance()->loadProxySettings();
+}
+
+ProxySettingsPage::ProxySettingsPage(QObject *parent) :
+    ISettingsPage(QLatin1String("Proxy"), QLatin1String("WebView"), parent)
+{
+}
+
+QWidget *ProxySettingsPage::createPage(QWidget *parent)
+{
+    return new ProxySettingsWidget(parent);
+}
---
+++ src/plugins/webviewplugin/proxysettings.h
@@ -0,0 +1,52 @@
+#ifndef PROXYSETTINGS_H
+#define PROXYSETTINGS_H
+
+#include <QtGui/QWidget>
+
+class QAbstractButton;
+class QSettings;
+
+namespace Ui {
+class ProxySettingsWidget;
+}
+
+class ProxySettingsWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ProxySettingsWidget(QWidget *parent = 0);
+    ~ProxySettingsWidget();
+
+private slots:
+    void onClick(QAbstractButton*);
+    void makeDirty();
+
+private:
+    void loadSettings();
+    void saveSettings();
+
+private:
+    Ui::ProxySettingsWidget *ui;
+    QSettings *m_settings;
+};
+
+#include <coreplugin/isettingspage.h>
+
+class ProxySettingsPage : public CorePlugin::ISettingsPage
+{
+    Q_OBJECT
+
+public:
+    explicit ProxySettingsPage(QObject *parent = 0);
+
+    QString name() const { return tr("Proxy"); }
+    QIcon icon() const { return QIcon(); }
+
+    QString categoryName() const { return tr("Web view"); }
+    QIcon categoryIcon() const { return QIcon(); }
+
+    QWidget *createPage(QWidget *parent);
+};
+
+#endif // PROXYSETTINGS_H
