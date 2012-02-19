b7ce9323b4bedcb6ef10b63d73b554ff8918056c
Ivan Komissarov <ABBAPOH@me.com>
2012-02-19 22:38:37 +0400

    Add russian web view translation

---
+++ src/plugins/webview/translations/webviewplugin_ru.ts
@@ -0,0 +1,398 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AppearanceSettingsPage</name>
+    <message>
+        <location filename="../appearancesettings.h" line="56"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.h" line="59"/>
+        <source>Web view</source>
+        <translation>Веб браузер</translation>
+    </message>
+</context>
+<context>
+    <name>AppearanceSettingsWidget</name>
+    <message>
+        <location filename="../appearancesettings.ui" line="14"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.ui" line="29"/>
+        <source>Standard font:</source>
+        <translation>Стандартный шрифт:</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.ui" line="39"/>
+        <source>Fixed-width font:</source>
+        <translation>Моноширинный шрифт:</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.ui" line="49"/>
+        <source>Minimum Font size:</source>
+        <translation>Минимальный размер шрифта:</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.ui" line="75"/>
+        <location filename="../appearancesettings.ui" line="82"/>
+        <source>Select...</source>
+        <translation>Выбрать...</translation>
+    </message>
+    <message>
+        <location filename="../appearancesettings.ui" line="166"/>
+        <source>Default encoding:</source>
+        <translation>Кодировка по умолчанию:</translation>
+    </message>
+</context>
+<context>
+    <name>CookieDialog</name>
+    <message>
+        <location filename="../cookiedialog.ui" line="14"/>
+        <source>Cookies</source>
+        <translation>Cookies</translation>
+    </message>
+    <message>
+        <location filename="../cookiedialog.ui" line="63"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../cookiedialog.ui" line="70"/>
+        <source>Remove all Cookies</source>
+        <translation>Удалить все файлы cookie</translation>
+    </message>
+</context>
+<context>
+    <name>CookieExceptionsDialog</name>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="14"/>
+        <source>Cookie Exceptions</source>
+        <translation>Исключения cookie</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="20"/>
+        <source>New Exception</source>
+        <translation>Новое исключение</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="28"/>
+        <source>Domain:</source>
+        <translation>Домен:</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="58"/>
+        <source>Block</source>
+        <translation>Блокировать</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="68"/>
+        <source>Allow For Session</source>
+        <translation>Разрешать на сессию</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="78"/>
+        <source>Allow</source>
+        <translation>Разрешать</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="90"/>
+        <source>Exceptions</source>
+        <translation>Исключения</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="134"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsdialog.ui" line="141"/>
+        <source>Remove all</source>
+        <translation>Удалить все</translation>
+    </message>
+</context>
+<context>
+    <name>CookieExceptionsModel</name>
+    <message>
+        <location filename="../cookieexceptionsmodel.cpp" line="63"/>
+        <source>Website</source>
+        <translation>Сайт</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsmodel.cpp" line="64"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsmodel.cpp" line="83"/>
+        <source>Allow</source>
+        <translation>Разрешено</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsmodel.cpp" line="90"/>
+        <source>Block</source>
+        <translation>Блокировано</translation>
+    </message>
+    <message>
+        <location filename="../cookieexceptionsmodel.cpp" line="97"/>
+        <source>Allow For Session</source>
+        <translation>Разрешено на сессию</translation>
+    </message>
+</context>
+<context>
+    <name>CookieModel</name>
+    <message>
+        <location filename="../cookiemodel.cpp" line="87"/>
+        <source>Website</source>
+        <translation>Сайт</translation>
+    </message>
+    <message>
+        <location filename="../cookiemodel.cpp" line="88"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="../cookiemodel.cpp" line="89"/>
+        <source>Path</source>
+        <translation>Путь</translation>
+    </message>
+    <message>
+        <location filename="../cookiemodel.cpp" line="90"/>
+        <source>Secure</source>
+        <translation>Безопасен</translation>
+    </message>
+    <message>
+        <location filename="../cookiemodel.cpp" line="91"/>
+        <source>Expires</source>
+        <translation>Истекает</translation>
+    </message>
+    <message>
+        <location filename="../cookiemodel.cpp" line="92"/>
+        <source>Contents</source>
+        <translation>Содержимое</translation>
+    </message>
+</context>
+<context>
+    <name>PrivacySettingsPage</name>
+    <message>
+        <location filename="../privacysettings.h" line="66"/>
+        <source>Privacy</source>
+        <translation>Конфиденциальность</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.h" line="69"/>
+        <source>Web View</source>
+        <translation>Веб браузер</translation>
+    </message>
+</context>
+<context>
+    <name>PrivacySettingsWidget</name>
+    <message>
+        <location filename="../privacysettings.ui" line="14"/>
+        <source>Privacy settings</source>
+        <translation>Настройки безопасности</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="39"/>
+        <source>Web Content</source>
+        <translation>Web-содержимое</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="45"/>
+        <source>Block Popup Windows</source>
+        <translation>Блокировать всплывающие окна</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="55"/>
+        <source>Enable Plugins</source>
+        <translation>Включить плагины</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="65"/>
+        <source>Enable Javascript</source>
+        <translation>Включить Javascript</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="75"/>
+        <source>View Images</source>
+        <translation>Показывать изображения</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="85"/>
+        <source>Persistent Data Storage</source>
+        <translation>Постоянное временное хранилище</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="117"/>
+        <source>Cookies</source>
+        <translation>Cookies</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="123"/>
+        <source>Accept Cookies:</source>
+        <translation>Принимать Cookies:</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="140"/>
+        <source>Always</source>
+        <translation>Всегда</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="145"/>
+        <source>Never</source>
+        <translation>Никогда</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="150"/>
+        <source>Only from sites you navigate to</source>
+        <translation>Только с сайтов, на которые осуществляется навигация</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="158"/>
+        <source>Exceptions...</source>
+        <translation>Исключения...</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="165"/>
+        <source>Keep Cookies Until:</source>
+        <translation>Хранить Cookies до:</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="182"/>
+        <source>They expire</source>
+        <translation>Они не истекут</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="187"/>
+        <source>I exit the application</source>
+        <translation>До выхода из приложения</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="192"/>
+        <source>At most 90 days</source>
+        <translation>Не больше 90 дней</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="200"/>
+        <source>Cookies...</source>
+        <translation>Cookies...</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="213"/>
+        <source>Filter Tracking Cookies</source>
+        <translation>Фильтровать &quot;Tracking Cookies&quot;</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="220"/>
+        <source>A cookie session ends:</source>
+        <translation>Сессия истекает:</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="237"/>
+        <source>When I exit the application</source>
+        <translation>При выходе из приложения</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="242"/>
+        <source>1 day</source>
+        <translation>1 день</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="247"/>
+        <source>2 days</source>
+        <translation>2 дня</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="252"/>
+        <source>3 days</source>
+        <translation>3 дня</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="257"/>
+        <source>7 days</source>
+        <translation>7 дней</translation>
+    </message>
+    <message>
+        <location filename="../privacysettings.ui" line="262"/>
+        <source>30 days</source>
+        <translation>30 дней</translation>
+    </message>
+</context>
+<context>
+    <name>ProxySettingsPage</name>
+    <message>
+        <location filename="../proxysettings.h" line="45"/>
+        <source>Proxy</source>
+        <translation>Прокси</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.h" line="48"/>
+        <source>Web view</source>
+        <translation>Веб браузер</translation>
+    </message>
+</context>
+<context>
+    <name>ProxySettingsWidget</name>
+    <message>
+        <location filename="../proxysettings.ui" line="14"/>
+        <source>Proxy settings</source>
+        <translation>Настройки прокси</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="20"/>
+        <source>Use proxy server</source>
+        <translation>Использовать прокси-сервер</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="32"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="43"/>
+        <source>Socks5</source>
+        <translation>Socks5</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="48"/>
+        <source>Http (Secure)</source>
+        <translation>Http (безопасный)</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="53"/>
+        <source>Http (Transparent)</source>
+        <translation>Http (прозрачный)</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="61"/>
+        <source>Proxy server:</source>
+        <translation>Прокси-сервер:</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="134"/>
+        <source>Server requires password</source>
+        <translation>Сервер требует пароль</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="143"/>
+        <source>User Name:</source>
+        <translation>Имя пользователя:</translation>
+    </message>
+    <message>
+        <location filename="../proxysettings.ui" line="160"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+</context>
+<context>
+    <name>WebView::WebViewEditor</name>
+    <message>
+        <location filename="../webvieweditor.cpp" line="164"/>
+        <source>Find</source>
+        <translation>Найти</translation>
+    </message>
+</context>
+</TS>
