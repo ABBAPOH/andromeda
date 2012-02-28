3a218c8980353cc9cd31cf8a5224be3575cb1f6e
Ivan Komissarov <ABBAPOH@me.com>
2012-02-28 23:03:59 +0400

    Add Widgets' ru translation

---
+++ src/libs/widgets/translations/widgets_ru.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>FilterLineEdit</name>
+    <message>
+        <location filename="../filterlineedit.cpp" line="23"/>
+        <source>Filter</source>
+        <translation>Фильтр</translation>
+    </message>
+    <message>
+        <location filename="../filterlineedit.cpp" line="24"/>
+        <source>Clear text</source>
+        <translation>Очистить текст</translation>
+    </message>
+</context>
+</TS>
