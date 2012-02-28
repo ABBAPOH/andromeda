8b7c8bca6ee933607c3389200ca62c8982f9ffde
ABBAPOH <ABBAPOH@nextmail.ru>
2012-02-28 22:30:08 +0400

    Add ExtensionSystem ru translation

---
+++ src/libs/extensionsystem/translations/extensionsystem_ru.ts
@@ -0,0 +1,473 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ExtensionSystem::PluginManager</name>
+    <message>
+        <location filename="../pluginmanager.cpp" line="293"/>
+        <source>No plugins found in (%1)</source>
+        <translation>Плагины не найдены в %1</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="299"/>
+        <source>Error parsing options : &apos;%1&apos;</source>
+        <translation>Ошибка при разборе параметров : &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="333"/>
+        <source>Failed to read spec file %1 : &apos;%2&apos;</source>
+        <translation>Невозможно прочитать файл спецификации %1 : &apos;%2&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>ExtensionSystem::PluginSpec</name>
+    <message>
+        <location filename="../pluginspec.cpp" line="107"/>
+        <source>Failed to initialize plugin %1</source>
+        <translation>Невозможно инициализировать плагин %1</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="131"/>
+        <source>Can&apos;t load plugin: </source>
+        <translation>Невозможно загрузить плагин:</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="137"/>
+        <source>Can&apos;t load plugin: not a valid plugin</source>
+        <translation>Невозможно загрузить плагин: плагин не является валидным</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="157"/>
+        <source>Can&apos;t unload plugin: %1 is not loaded</source>
+        <translation>Невозможно выгрузить плагин: %1 не загружен</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="186"/>
+        <source>Can&apos;t unload plugin library: %1</source>
+        <translation>Невозможно выгрузить библиотеку: %1</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="217"/>
+        <source>Can&apos;t resolve dependency &apos;%1(%2)&apos;</source>
+        <translation>Невозможно разрешить зависимость &apos;%1(%2)&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="559"/>
+        <source>No error</source>
+        <comment>PluginSpec</comment>
+        <translation>Нет ошибки</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="590"/>
+        <location filename="../pluginspec.cpp" line="648"/>
+        <source>Failed to open file %1 : &apos;%2&apos;</source>
+        <translation>Невозможно открыть файл %1 : &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="599"/>
+        <source>Cannot read file %1 : &apos;%2&apos;</source>
+        <translation>Невозможно прочесть файл %1 : &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="618"/>
+        <source>Failed to add option %1 : &apos;%2&apos;</source>
+        <translation>Невозможно добавить параметр %1 : &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspec.cpp" line="655"/>
+        <source>Failed to write file %1 : &apos;%2&apos;</source>
+        <translation>Невозможно записать файл %1 : &apos;%2&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>ExtensionSystem::PluginViewModel</name>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="98"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="99"/>
+        <source>Loads on Startup</source>
+        <translation>Загружать на старте</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="100"/>
+        <source>Enabled</source>
+        <translation>Включён</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="101"/>
+        <source>Version</source>
+        <translation>Версия</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="102"/>
+        <source>Compatibility Version</source>
+        <translation>Совместимая версия</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="103"/>
+        <source>Vendor</source>
+        <translation>Поставщик</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="104"/>
+        <source>Url</source>
+        <translation>Адрес</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="105"/>
+        <source>Library Path</source>
+        <translation>Путь к библиотеке</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="106"/>
+        <source>Description</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="107"/>
+        <source>Copyright</source>
+        <translation>Авторское право</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="108"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../pluginviewmodel.cpp" line="109"/>
+        <source>Dependencies</source>
+        <translation>Зависимости</translation>
+    </message>
+</context>
+<context>
+    <name>FullPluginView</name>
+    <message>
+        <location filename="../fullpluginview.ui" line="14"/>
+        <source>Plugin Info</source>
+        <translation>Информация о плагине</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="20"/>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="27"/>
+        <location filename="../fullpluginview.ui" line="41"/>
+        <location filename="../fullpluginview.ui" line="55"/>
+        <location filename="../fullpluginview.ui" line="69"/>
+        <location filename="../fullpluginview.ui" line="83"/>
+        <location filename="../fullpluginview.ui" line="127"/>
+        <source>N/A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="34"/>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="48"/>
+        <source>Compatibility version:</source>
+        <translation>Совместимая версия:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="62"/>
+        <source>Vendor:</source>
+        <translation>Поставщик:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="76"/>
+        <source>Url:</source>
+        <translation>Адрес:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="90"/>
+        <source>Location:</source>
+        <translation>Расположение:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="97"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="120"/>
+        <source>Copyright:</source>
+        <translation>Авторское право:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="134"/>
+        <source>License:</source>
+        <translation>Лицензия:</translation>
+    </message>
+    <message>
+        <location filename="../fullpluginview.ui" line="170"/>
+        <source>Dependencies:</source>
+        <translation>Зависимости:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="30"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="69"/>
+        <location filename="../plugineditor/mainwindow.cpp" line="87"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="82"/>
+        <source>PluginEditor</source>
+        <translation>Редактор плагинов</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="84"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="86"/>
+        <source>Open...</source>
+        <translation>Открыть...</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="88"/>
+        <source>Save As...</source>
+        <translation>Сохранить как...</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="90"/>
+        <source>Quit PluginEditor</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="94"/>
+        <source>Format</source>
+        <translation>Формат</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="100"/>
+        <source>Xml format</source>
+        <translation>Формат xml</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor/mainwindow.cpp" line="106"/>
+        <source>Binary format</source>
+        <translation>Бинарный формат</translation>
+    </message>
+</context>
+<context>
+    <name>PluginEditor</name>
+    <message>
+        <location filename="../plugineditor.ui" line="14"/>
+        <source>Plugin Info</source>
+        <translation>Информация о плагине</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="20"/>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="27"/>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="34"/>
+        <source>Compatibility version:</source>
+        <translation>Совместимая версия:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="41"/>
+        <source>Vendor:</source>
+        <translation>Поставщик:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="48"/>
+        <source>Url:</source>
+        <translation>Адрес:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="55"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="82"/>
+        <source>License:</source>
+        <translation>Лицензия:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="109"/>
+        <source>Dependencies:</source>
+        <translation>Зависимости:</translation>
+    </message>
+    <message>
+        <location filename="../plugineditor.ui" line="147"/>
+        <source>Copyright:</source>
+        <translation>Авторское право:</translation>
+    </message>
+</context>
+<context>
+    <name>PluginView</name>
+    <message>
+        <location filename="../pluginview.ui" line="14"/>
+        <source>Plugins</source>
+        <translation>Плагины</translation>
+    </message>
+    <message>
+        <location filename="../pluginview.ui" line="25"/>
+        <source>More...</source>
+        <translation>Подробнее...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../options.cpp" line="69"/>
+        <source>Already have option with name %1</source>
+        <translation>Параметр с именем %1 уже существует</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="74"/>
+        <source>Already have option with short name %1</source>
+        <translation>Параметр с коротким именем %1 уже существует</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="84"/>
+        <source>Option %1 has duplicate value names</source>
+        <translation>Параметр %1 имеет два одинаковых имени</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="113"/>
+        <location filename="../pluginspecformathandler_p.h" line="25"/>
+        <source>No error</source>
+        <translation>Нет ошибки</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="171"/>
+        <location filename="../options.cpp" line="268"/>
+        <location filename="../options.cpp" line="285"/>
+        <source>Redeclaration of option %1</source>
+        <translation>Повторное определение параметра %1</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="180"/>
+        <location filename="../options.cpp" line="293"/>
+        <source>Not enough arguments for option %1</source>
+        <translation>Недостаточно аргументов для параметра %1</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="188"/>
+        <source>Unknown option %1</source>
+        <translation>Неизвестный параметр %1</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="207"/>
+        <source>Too many arguments for option %1</source>
+        <translation>Слишком много аргументов для параметра %1</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="234"/>
+        <source>Unknown type</source>
+        <translation>Неизвестный тип</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="236"/>
+        <source>Unsupported type %1</source>
+        <translation>Тип %1 не поддерживается</translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="242"/>
+        <source>Type mismatch for argument %1 (expected %2)</source>
+        <translation>Несовпадение типов для аргумента %1 (ожидается %2)</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="27"/>
+        <source>Cannot read binary file format</source>
+        <translation>Невозможно прочитать файл в бинарном формате</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="149"/>
+        <source>Error parsing xml stream: %1, at line %2, column %3</source>
+        <translation>Ошибка разбора xml: %1 на строке %2, колонка %3</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="242"/>
+        <location filename="../pluginspecformathandler.cpp" line="296"/>
+        <location filename="../pluginspecformathandler.cpp" line="365"/>
+        <source>Unexpected token &apos;%1&apos;</source>
+        <translation>Неожиданный токен &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="250"/>
+        <location filename="../pluginspecformathandler.cpp" line="317"/>
+        <location filename="../pluginspecformathandler.cpp" line="390"/>
+        <source>Unknown element &apos;%1&apos;</source>
+        <translation>Неизвестный элемент &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="265"/>
+        <source>Expected &lt;/dependencyList&gt;</source>
+        <translation>Ожидается &lt;/dependencyList&gt;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="274"/>
+        <location filename="../pluginspecformathandler.cpp" line="341"/>
+        <location filename="../pluginspecformathandler.cpp" line="414"/>
+        <location filename="../pluginspecformathandler.cpp" line="468"/>
+        <source>Unexpected character sequence</source>
+        <translation>Неожиданная последовательность символов</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="278"/>
+        <location filename="../pluginspecformathandler.cpp" line="345"/>
+        <location filename="../pluginspecformathandler.cpp" line="418"/>
+        <location filename="../pluginspecformathandler.cpp" line="472"/>
+        <location filename="../pluginspecformathandler.cpp" line="520"/>
+        <source>Unexpected token</source>
+        <translation>Неожиданный токен</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="307"/>
+        <location filename="../pluginspecformathandler.cpp" line="385"/>
+        <source>Unknown type %1</source>
+        <translation>Неизвестный тип %1</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="332"/>
+        <source>Expected &lt;/option&gt;</source>
+        <translation>Ожидается &lt;/option&gt;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="405"/>
+        <source>Expected &lt;/options&gt;</source>
+        <translation>Ожидается &lt;/options&gt;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="450"/>
+        <location filename="../pluginspecformathandler.cpp" line="500"/>
+        <source>Unknown element %1</source>
+        <translation>Неизвестный элемент %1</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="459"/>
+        <source>Expected &lt;/plugin&gt;</source>
+        <translation>Ожидается &lt;/plugin&gt;</translation>
+    </message>
+    <message>
+        <location filename="../pluginspecformathandler.cpp" line="512"/>
+        <source>Expected end of document</source>
+        <translation>Ожидается конец документа</translation>
+    </message>
+</context>
+</TS>
