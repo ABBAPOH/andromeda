b53991b27935f86df3481ffe9b960d96b6983caf
fr33mind <freemind@lavabit.com>
2012-02-29 16:35:25 +0000

    Added translation to portuguese (pt_PT).

---
+++ src/libs/guisystem/translations/guisystem_pt.ts
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>CommandsSettingsWidget</name>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="14"/>
+        <source>Commands Settings</source>
+        <translation>Definições de Comandos</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="44"/>
+        <source>Reset</source>
+        <translation>Reverter</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="51"/>
+        <source>Reset all to default</source>
+        <translation>Reverter tudo para valores por omissão</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="54"/>
+        <source>Reset All</source>
+        <translation>Reverter Tudo</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="74"/>
+        <source>Import</source>
+        <translation>Importar</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.ui" line="81"/>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+</context>
+<context>
+    <name>GuiSystem::CommandsModel</name>
+    <message>
+        <location filename="../commandsmodel.cpp" line="155"/>
+        <source>Command</source>
+        <translation>Comando</translation>
+    </message>
+    <message>
+        <location filename="../commandsmodel.cpp" line="156"/>
+        <source>Shortcut</source>
+        <translation>Atalho</translation>
+    </message>
+</context>
+<context>
+    <name>GuiSystem::CommandsSettingsWidget</name>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="173"/>
+        <source>Export shortcuts</source>
+        <translation>Exportar atalhos</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="175"/>
+        <location filename="../commandssettingswidget.cpp" line="195"/>
+        <source>Keyboard mappings scheme (*.kms)</source>
+        <translation>Esquema de mapeamentos de teclado (*.kms)</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="182"/>
+        <location filename="../commandssettingswidget.cpp" line="202"/>
+        <location filename="../commandssettingswidget.cpp" line="210"/>
+        <source>Warning</source>
+        <translation>Aviso</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="183"/>
+        <source>Unable to open file %1 for writing.</source>
+        <translation>Não foi possível abrir ficheiro %1 para escrita.</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="193"/>
+        <source>Import shortcuts</source>
+        <translation>Importar atalhos</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="203"/>
+        <source>Unable to open file %1 for reading.</source>
+        <translation>Não foi possível abrir ficheiro %1 para leitura.</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingswidget.cpp" line="211"/>
+        <source>Unable to import shortcuts from file %1.</source>
+        <translation>Não foi possível importar atalhos do ficheiro %1.</translation>
+    </message>
+</context>
+<context>
+    <name>GuiSystem::FindToolBar</name>
+    <message>
+        <location filename="../findtoolbar.cpp" line="287"/>
+        <source>Case Sensitive</source>
+        <translation>Sensível a Maíusculas/Minúsculas</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="288"/>
+        <source>Whole Words Only</source>
+        <translation>Apenas Palavras Completas</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="289"/>
+        <source>Use Regular Expressions</source>
+        <translation>Usar Expressões Regulares</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="291"/>
+        <source>Find</source>
+        <translation>Procurar</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="292"/>
+        <source>Find previous</source>
+        <translation>Procurar anterior</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="293"/>
+        <source>Find next</source>
+        <translation>Procurar próximo</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="295"/>
+        <location filename="../findtoolbar.cpp" line="296"/>
+        <location filename="../findtoolbar.cpp" line="297"/>
+        <source>Replace</source>
+        <translation>Substituir</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="298"/>
+        <location filename="../findtoolbar.cpp" line="299"/>
+        <source>Replace next</source>
+        <translation>Substituir próximo</translation>
+    </message>
+    <message>
+        <location filename="../findtoolbar.cpp" line="301"/>
+        <location filename="../findtoolbar.cpp" line="302"/>
+        <source>Replace all</source>
+        <translation>Substituir tudo</translation>
+    </message>
+</context>
+<context>
+    <name>GuiSystem::MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="301"/>
+        <source>Save as</source>
+        <translation>Guardar como</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="405"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="406"/>
+        <source>Save as...</source>
+        <translation>Guardar como...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="407"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="408"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="409"/>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="410"/>
+        <source>Forward</source>
+        <translation>Avançar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="412"/>
+        <source>Next editor</source>
+        <translation>Próximo editor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="413"/>
+        <source>Previous editor</source>
+        <translation>Editor anterior</translation>
+    </message>
+</context>
+<context>
+    <name>GuiSystem::SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.cpp" line="187"/>
+        <source>Preferences</source>
+        <translation>Preferências</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/core/translations/coreplugin_pt.ts
@@ -0,0 +1,303 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>CommandsSettingsPage</name>
+    <message>
+        <location filename="../commandssettingspage.cpp" line="14"/>
+        <source>Shortcut settings</source>
+        <translation>Preferências dos atalhos</translation>
+    </message>
+    <message>
+        <location filename="../commandssettingspage.cpp" line="24"/>
+        <source>General</source>
+        <translation>Geral</translation>
+    </message>
+</context>
+<context>
+    <name>Core::BrowserWindowPrivate</name>
+    <message>
+        <location filename="../browserwindow.cpp" line="43"/>
+        <source>Up one level</source>
+        <translation>Subir um nível</translation>
+    </message>
+</context>
+<context>
+    <name>Core::MyAddressBar</name>
+    <message>
+        <location filename="../addressbar.cpp" line="46"/>
+        <source>Redo</source>
+        <translation>Refazer</translation>
+    </message>
+    <message>
+        <location filename="../addressbar.cpp" line="47"/>
+        <source>Undo</source>
+        <translation>Desfazer</translation>
+    </message>
+    <message>
+        <location filename="../addressbar.cpp" line="49"/>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <location filename="../addressbar.cpp" line="50"/>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <location filename="../addressbar.cpp" line="51"/>
+        <source>Paste</source>
+        <translation>Colar</translation>
+    </message>
+    <message>
+        <location filename="../addressbar.cpp" line="52"/>
+        <source>Select all</source>
+        <translation>Seleccionar tudo</translation>
+    </message>
+</context>
+<context>
+    <name>Core::TabContainer</name>
+    <message>
+        <location filename="../tabcontainer.cpp" line="138"/>
+        <location filename="../tabcontainer.cpp" line="140"/>
+        <location filename="../tabcontainer.cpp" line="184"/>
+        <location filename="../tabcontainer.cpp" line="186"/>
+        <source>New tab</source>
+        <translation>Novo separador</translation>
+    </message>
+</context>
+<context>
+    <name>CorePlugin</name>
+    <message>
+        <location filename="../coreplugin.cpp" line="207"/>
+        <source>Couldn&apos;t restore session (located at %1)</source>
+        <translation>Não foi possível restaurar a sessão (localizada em %1)</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="253"/>
+        <source>&lt;h3&gt;Andromeda %1&lt;/h3&gt;&lt;br/&gt;Revision %2&lt;br/&gt;&lt;br/&gt;Copyright 2010-2011 %3&lt;br/&gt;Bugreports send to %4&lt;br/&gt;&lt;br/&gt;This is alpha version.&lt;br/&gt;&lt;br/&gt;The program is provided as is with no warranty of any kind, including the warranty of design, merchantability and fitness for a particular purpose.</source>
+        <translation>&lt;h3&gt;Andromeda %1&lt;/h3&gt;&lt;br/&gt;Revisão %2&lt;br/&gt;&lt;br/&gt;Copyright 2010-2011 %3&lt;br/&gt;Enviar erros para %4&lt;br/&gt;&lt;br/&gt;Esta é uma versão alpha.&lt;br/&gt;&lt;br/&gt; Este programa é distribuido na esperança que possa ser  util, mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="271"/>
+        <source>About Andromeda</source>
+        <translation>Acerca de Andromeda</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="316"/>
+        <source>File</source>
+        <translation>Ficheiro</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="320"/>
+        <source>New window</source>
+        <translation>Nova janela</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="324"/>
+        <source>New tab</source>
+        <translation>Novo separador</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="328"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="335"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="339"/>
+        <source>Save as...</source>
+        <translation>Guardar como...</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="346"/>
+        <source>Quit Andromeda</source>
+        <translation>Sair de Andromeda</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="363"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="367"/>
+        <source>Undo</source>
+        <translation>Dezfazer</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="371"/>
+        <source>Redo</source>
+        <translation>Refazer</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="378"/>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="382"/>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="386"/>
+        <source>Paste</source>
+        <translation>Colar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="389"/>
+        <source>Select All</source>
+        <translation>Seleccionar tudo</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="395"/>
+        <source>Find</source>
+        <translation>Procurar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="398"/>
+        <source>Find next</source>
+        <translation>Procurar próximo</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="401"/>
+        <source>Find previous</source>
+        <translation>Procurar anterior</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="412"/>
+        <source>View</source>
+        <translation>Vista</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="426"/>
+        <source>Go to</source>
+        <translation>Ir para</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="430"/>
+        <source>Back</source>
+        <translation>Recuar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="434"/>
+        <source>Forward</source>
+        <translation>Avançar</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="438"/>
+        <source>Up one level</source>
+        <translation>Subir um nível</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="449"/>
+        <source>Tools</source>
+        <translation>Ferramentas</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="453"/>
+        <source>Plugins...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="458"/>
+        <source>View all settings...</source>
+        <translation>Ver todas as preferências...</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="466"/>
+        <source>Preferences</source>
+        <translation>Preferências</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="484"/>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="487"/>
+        <source>About Andromeda...</source>
+        <translation>Acerca de Andromeda...</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="494"/>
+        <source>About Qt...</source>
+        <translation>Acerca do Qt...</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="513"/>
+        <source>Dock menu</source>
+        <translation>Menu da dock</translation>
+    </message>
+    <message>
+        <location filename="../coreplugin.cpp" line="515"/>
+        <source>Tray menu</source>
+        <translation>Menu da área de notificação</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsModel</name>
+    <message>
+        <location filename="../settingsmodel.cpp" line="233"/>
+        <source>Key</source>
+        <translation>Chave</translation>
+    </message>
+    <message>
+        <location filename="../settingsmodel.cpp" line="234"/>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <location filename="../settingsmodel.cpp" line="235"/>
+        <source>Value</source>
+        <translation>Valor</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../settingswidget.ui" line="14"/>
+        <source>Settings Editor</source>
+        <translation>Editor de Definições</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="29"/>
+        <source>Remove selected row</source>
+        <translation>Remover linha seleccionada</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="32"/>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="39"/>
+        <source>Clears settings</source>
+        <translation>Apaga as definições</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="42"/>
+        <source>Remove all</source>
+        <translation>Remover tudo</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="62"/>
+        <source>Refreshes tree view, synchronising it with settings</source>
+        <translation>Actualiza a vista, sincronizando-a com as definições</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="65"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.cpp" line="13"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+</context>
+</TS>
