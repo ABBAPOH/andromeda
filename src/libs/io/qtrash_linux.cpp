1ee98d5e7923ef8217064ddd83bbcdb1681f849e
ABBAPOH <ABBAPOH@nextmail.ru>
2012-05-02 20:11:59 +0400

    Add qxdg

--- src/libs/io/qtrash_linux.cpp
+++ src/libs/io/qtrash_linux.cpp
@@ -13,40 +13,7 @@
 
 #include "qdriveinfo.h"
 #include "qtrashfileinfo_p.h"
-
-static inline QString homePath()
-{
-    return QString(qgetenv("HOME"));
-}
-
-static inline void fixBashShortcuts(QString &s)
-{
-    if (s.startsWith('~'))
-        s = QString(qgetenv("HOME")) + (s).mid(1);
-}
-
-static QString xdgSingleDir(const QString &envVar, const QString &def, bool createDir)
-{
-    QString s(qgetenv(envVar.toAscii()));
-
-    if (!s.isEmpty())
-        fixBashShortcuts(s);
-    else
-        s = QString("%1/%2").arg(qgetenv("HOME"), def);
-
-    QDir d(s);
-    if (createDir && !d.exists()) {
-        if (!d.mkpath("."))
-            qWarning() << QString("Can't create %1 directory.").arg(d.absolutePath());
-    }
-
-    return d.absolutePath();
-}
-
-static inline QString dataHome(bool createDir)
-{
-    return xdgSingleDir("XDG_DATA_HOME", ".local/share", createDir);
-}
+#include "qxdg.h"
 
 static inline QString getHomeTrash()
 {
---
+++ src/libs/io/qxdg.h
@@ -0,0 +1,8 @@
+#ifndef QXDG_H
+#define QXDG_H
+
+#include <QtCore/QString>
+
+QString dataHome(bool createDir = false);
+
+#endif // QXDG_H
