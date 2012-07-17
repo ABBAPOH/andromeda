c38a5d7da83b1c21c8369877f450094fdc8a0b39
Ivan Komissarov <ABBAPOH@gmail.com>
2012-07-18 00:53:12 +0400

    IPlugin doc fixes

--- src/libs/extensionsystem/iplugin.cpp
+++ src/libs/extensionsystem/iplugin.cpp
@@ -15,8 +15,12 @@ public:
 using namespace ExtensionSystem;
 
 /*!
-    \fn IPlugin::IPlugin()
-    \internal
+    \class ExtensionSystem::IPlugin
+    \brief IPlugin is a base class for user plugins.
+*/
+
+/*!
+    \brief Creates an IPlugin.
 */
 IPlugin::IPlugin() :
     QObject(),
@@ -25,8 +29,7 @@ IPlugin::IPlugin() :
 }
 
 /*!
-    \fn IPlugin::~IPlugin()
-    \internal
+  \brief Destroys IPlugin.
 */
 IPlugin::~IPlugin()
 {
@@ -43,8 +46,31 @@ IPlugin::~IPlugin()
 
 /*!
     \fn bool IPlugin::initialize()
-    \brief Function for primary initialization of plugin.
-    This function called after all dependencies are initialized.
+    \brief Reiplement to perform primary initialization of a plugin.
+
+    This function called after plugin was loaded and after all dependencies are initialized.
+*/
+
+/*!
+    \fn void IPlugin::postInitialize(const QVariantMap &options)
+    \brief Reiplement to perform secondary initialization of a plugin.
+
+    Passed \a options are the command line arguments for this plugin. They only
+    include keys described in plugin's spec file.
+
+    For a multiple-instance applications, this function is called after all
+    plugins were initialized.
+
+    For a single-instance applications, this function is called for a first
+    instance each time new instance is trying to start (i.e. when QtSingleApplication
+    receives message from new instance).
+*/
+
+/*!
+    \fn bool IPlugin::shutdown()
+    \brief Reiplement to perform deinitialization of a plugin.
+
+    This function called before plugin will be unloaded.
 */
 
 QObject *IPlugin::object(const QString &name)
@@ -53,7 +79,6 @@ QObject *IPlugin::object(const QString &name)
 }
 
 /*!
-    \fn void addObject(QObject * object)
     \brief Adds \a object to object pool in PluginManager.
 */
 void IPlugin::addObject(QObject * object, const QString &name)
@@ -67,7 +92,6 @@ void IPlugin::addObject(QObject * object, const QString &name)
 }
 
 /*!
-    \fn void removeObject(QObject * object)
     \brief Removes \a object from object pool in PluginManager.
 */
 void IPlugin::removeObject(QObject * object)
