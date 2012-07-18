100237b238137a3cc0c4e561c8d2641901720e31
Ivan Komissarov <ABBAPOH@gmail.com>
2012-07-18 20:23:33 +0400

    Refactor FullPluginView

--- src/libs/extensionsystem/fullpluginview.cpp
+++ src/libs/extensionsystem/fullpluginview.cpp
@@ -10,7 +10,8 @@ using namespace ExtensionSystem;
 FullPluginView::FullPluginView(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::FullPluginView),
-    mapper(new QDataWidgetMapper(this))
+    m_mapper(new QDataWidgetMapper(this)),
+    m_model(0)
 {
     ui->setupUi(this);
 }
@@ -20,26 +21,36 @@ FullPluginView::~FullPluginView()
     delete ui;
 }
 
+PluginViewModel * FullPluginView::model() const
+{
+    return m_model;
+}
+
 void FullPluginView::setModel(PluginViewModel * model)
 {
-    this->model = model;
-    mapper->clearMapping();
-    mapper->setModel(model);
-    mapper->setRootIndex(model->index(0,0));
-    mapper->addMapping(ui->label_Name, 0, "text");
-    mapper->addMapping(ui->label_Version, 3, "text");
-    mapper->addMapping(ui->label_CompatibilityVersion, 4, "text");
-    mapper->addMapping(ui->label_Vendor, 5, "text");
-    mapper->addMapping(ui->label_Url, 6, "text");
-    mapper->addMapping(ui->lineEdit_Location, 7, "text");
-    mapper->addMapping(ui->textBrowser_Description, 8, "plainText");
-    mapper->addMapping(ui->label_Copyright, 9, "text");
-    mapper->addMapping(ui->textBrowser_License, 10, "plainText");
-    mapper->addMapping(ui->textBrowser_Dependencies, 11, "plainText");
+    if (m_model == model)
+        return;
+
+    m_model = model;
+    m_mapper->clearMapping();
+    m_mapper->setModel(m_model);
+    if (m_model != 0) {
+        m_mapper->setRootIndex(m_model->index(0,0));
+        m_mapper->addMapping(ui->label_Name, 0, "text");
+        m_mapper->addMapping(ui->label_Version, 3, "text");
+        m_mapper->addMapping(ui->label_CompatibilityVersion, 4, "text");
+        m_mapper->addMapping(ui->label_Vendor, 5, "text");
+        m_mapper->addMapping(ui->label_Url, 6, "text");
+        m_mapper->addMapping(ui->lineEdit_Location, 7, "text");
+        m_mapper->addMapping(ui->textBrowser_Description, 8, "plainText");
+        m_mapper->addMapping(ui->label_Copyright, 9, "text");
+        m_mapper->addMapping(ui->textBrowser_License, 10, "plainText");
+        m_mapper->addMapping(ui->textBrowser_Dependencies, 11, "plainText");
+    }
 }
 
 void FullPluginView::setIndex(const QModelIndex &index)
 {
-    mapper->setRootIndex(index.parent());
-    mapper->setCurrentModelIndex(index);
+    m_mapper->setRootIndex(index.parent());
+    m_mapper->setCurrentModelIndex(index);
 }
