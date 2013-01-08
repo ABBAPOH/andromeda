590358675f46d036bff4e957d37456c678a0d30b
Ivan Komissarov <ABBAPOH@me.com>
2013-01-08 17:49:37 +0400

    Fix BinEditor and ImageViewer translations.

--- src/plugins/bineditor/translations/bineditorplugin_ru.ts
+++ src/plugins/bineditor/translations/bineditorplugin_ru.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../bineditor.cpp" line="81"/>
         <source>Redo</source>
-        <translation type="unfinished"></translation>
+        <translation>Повторить</translation>
     </message>
     <message>
         <location filename="../bineditor.cpp" line="82"/>
         <source>Undo</source>
-        <translation type="unfinished"></translation>
+        <translation>Отменить</translation>
     </message>
     <message>
         <location filename="../bineditor.cpp" line="83"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать</translation>
     </message>
     <message>
         <location filename="../bineditor.cpp" line="84"/>
         <source>Select all</source>
-        <translation type="unfinished"></translation>
+        <translation>Выделить всё</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="../bineditor.cpp" line="109"/>
         <source>Binary editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Двоичный редактор</translation>
     </message>
 </context>
 <context>
@@ -38,138 +38,138 @@
         <location filename="../binedit.cpp" line="591"/>
         <location filename="../binedit.cpp" line="1643"/>
         <source>Cannot open device: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Невозможно открыть устройство: %2</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="593"/>
         <location filename="../binedit.cpp" line="1645"/>
         <source>Cannot open %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Невозможно открыть %1: %2</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1229"/>
         <source>Memory at 0x%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Память по адресу 0x%1</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1231"/>
         <source>Decimal&amp;nbsp;unsigned&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Десятичное&amp;nbsp;беззнаковое&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1232"/>
         <source>Decimal&amp;nbsp;signed&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Десятичное&amp;nbsp;знаковое&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1233"/>
         <source>Previous&amp;nbsp;decimal&amp;nbsp;unsigned&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее&amp;nbsp;десятичное&amp;nbsp;беззнаковое&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1234"/>
         <source>Previous&amp;nbsp;decimal&amp;nbsp;signed&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее&amp;nbsp;десятичное&amp;nbsp;знаковое&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1239"/>
         <source>%1-bit&amp;nbsp;Integer&amp;nbsp;Type</source>
-        <translation type="unfinished"></translation>
+        <translation>%1-бит&amp;nbsp;целочисленный&amp;nbsp;тип</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1240"/>
         <source>Little Endian</source>
-        <translation type="unfinished"></translation>
+        <translation>Little Endian</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1240"/>
         <source>Big Endian</source>
-        <translation type="unfinished"></translation>
+        <translation>Big Endian</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1266"/>
         <source>Binary&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Двоичное&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1273"/>
         <source>Octal&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Восьмеричное&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1286"/>
         <source>Previous&amp;nbsp;binary&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее&amp;nbsp;двоичное&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1292"/>
         <source>Previous&amp;nbsp;octal&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее&amp;nbsp;восьмеричное&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1308"/>
         <source>&lt;i&gt;double&lt;/i&gt;&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;i&gt;double&lt;/i&gt;&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1311"/>
         <source>Previous &lt;i&gt;double&lt;/i&gt;&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее &lt;i&gt;double&lt;/i&gt;&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1322"/>
         <source>&lt;i&gt;float&lt;/i&gt;&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;i&gt;float&lt;/i&gt;&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1325"/>
         <source>Previous &lt;i&gt;float&lt;/i&gt;&amp;nbsp;value:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущее &lt;i&gt;float&lt;/i&gt;&amp;nbsp;значение:</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1338"/>
         <source>Bin Edtor error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка двоичного редактора</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1462"/>
         <source>Copying Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Копирование не удалось</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1463"/>
         <source>You cannot copy more than 4 MB of binary data.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы не можете копировать больше, чем 4МБ двоичных данных.</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1581"/>
         <source>Copy Selection as ASCII Characters</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать выделенное как ASCII символы</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1582"/>
         <source>Copy Selection as Hex Values</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать выделенное как шестнадцатеричные значения</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1603"/>
         <source>Jump to Address in This Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Перейти по адресу в этом окне</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1604"/>
         <source>Jump to Address in New Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Перейти по адресу в новом окне</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1672"/>
         <source>Jump to Address 0x%1 in This Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Перейти по адресу 0x%1 в этом окне</translation>
     </message>
     <message>
         <location filename="../binedit.cpp" line="1674"/>
         <source>Jump to Address 0x%1 in New Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Перейти по адресу 0x%1 в новом окне</translation>
     </message>
 </context>
 </TS>
--- src/plugins/imageview/translations/imageviewplugin_ru.ts
+++ src/plugins/imageview/translations/imageviewplugin_ru.ts
@@ -6,62 +6,62 @@
     <message>
         <location filename="../imageviewplugin.cpp" line="33"/>
         <source>Image view</source>
-        <translation type="unfinished"></translation>
+        <translation>Просмотрщик изображений</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="36"/>
         <source>Move tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Инструмент перемещения</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="37"/>
         <source>Selection tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Инструмент выделения</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="40"/>
         <source>Zoom in</source>
-        <translation type="unfinished"></translation>
+        <translation>Увеличить</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="41"/>
         <source>Zoom out</source>
-        <translation type="unfinished"></translation>
+        <translation>Уменьшить</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="42"/>
         <source>Fit in view</source>
-        <translation type="unfinished"></translation>
+        <translation>Подогнать под размер окна</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="43"/>
         <source>Normal size</source>
-        <translation type="unfinished"></translation>
+        <translation>Исходный размер</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="46"/>
         <source>Rotate left</source>
-        <translation type="unfinished"></translation>
+        <translation>Повернуть налево</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="47"/>
         <source>Rotate right</source>
-        <translation type="unfinished"></translation>
+        <translation>Повернуть направо</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="48"/>
         <source>Flip horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Отразить горизонтально</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="49"/>
         <source>Flip vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Отразить вертикально</translation>
     </message>
     <message>
         <location filename="../imageviewplugin.cpp" line="50"/>
         <source>Reset original</source>
-        <translation type="unfinished"></translation>
+        <translation>Вернуть к оригиналу</translation>
     </message>
 </context>
 <context>
@@ -69,7 +69,7 @@
     <message>
         <location filename="../imagevieweditor.cpp" line="76"/>
         <source>Image viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Просмотрщик изображений</translation>
     </message>
 </context>
 </TS>
