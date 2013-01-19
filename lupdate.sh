05d7688a8173d11e0c6cbee15bbd9e68b13a34cb
Ivan Komissarov <ABBAPOH@me.com>
2013-01-19 14:33:58 +0400

    Add "plugins" suffix to plugins directories.

--- lupdate.sh
+++ lupdate.sh
@@ -1,24 +1,18 @@
-function update_lib {
+function update {
     filename=$(basename $1)
     echo "updating" $1/translations/${filename}_$2.ts
     lupdate $1/ -ts $1/translations/${filename}_$2.ts $3
 }
 
-function update_plugin {
-    filename=$(basename $1)
-    echo "updating" $1/translations/${filename}plugin_$2.ts
-    lupdate $1/ -ts $1/translations/${filename}plugin_$2.ts $3
-}
-
 function main {
     for lib in src/libs/* ; do
-        update_lib $lib $1 $2
+        update $lib $1 $2
     done
     for plugin in src/plugins/* ; do
-        update_plugin $plugin $1 $2
+        update $plugin $1 $2
     done
     for app in src/apps/* ; do
-        update_lib $app $1 $2
+        update $app $1 $2
     done
 }
 
