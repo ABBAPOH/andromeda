94d3bceb78bcc39510001f8ce7e7bed33e9bea61
Ivan Komissarov <ABBAPOH@me.com>
2013-04-30 11:34:57 +0400

    Add AbstractCommand.

---
+++ src/libs/guisystem/abstractcommand.h
@@ -0,0 +1,47 @@
+#ifndef ABSTRACTCOMMAND_H
+#define ABSTRACTCOMMAND_H
+
+#include "guisystem_global.h"
+
+#include <QtCore/QObject>
+#include <QtGui/QIcon>
+
+class QAction;
+
+namespace GuiSystem {
+
+class AbstractCommandPrivate;
+class GUISYSTEM_EXPORT AbstractCommand : public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(AbstractCommand)
+    Q_DECLARE_PRIVATE(AbstractCommand)
+
+    Q_PROPERTY(QByteArray id READ id)
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY changed)
+    Q_PROPERTY(QIcon icon READ icon WRITE setIcon NOTIFY changed)
+public:
+    ~AbstractCommand();
+
+    QByteArray id() const;
+    QString text() const;
+    QIcon icon() const;
+
+public slots:
+    void setText(const QString &text);
+    void setIcon(const QIcon &icon);
+
+signals:
+    void changed();
+
+protected:
+    explicit AbstractCommand(const QByteArray &id, QObject *parent = 0);
+    explicit AbstractCommand(AbstractCommandPrivate &dd, const QByteArray &id, QObject *parent = 0);
+
+protected:
+    AbstractCommandPrivate *d_ptr;
+};
+
+} // namespace GuiSystem
+
+#endif // ABSTRACTCOMMAND_H
