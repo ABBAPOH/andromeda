42c424de5bcfc2b024eb1a50ead9b2b3f3b4331a
Ivan Komissarov <ABBAPOH@gmail.com>
2014-01-06 22:52:57 +0400

    Add GeneralSettingsWidget doc.

--- src/app/generalsettingswidget.cpp
+++ src/app/generalsettingswidget.cpp
@@ -5,6 +5,14 @@
 
 using namespace Andromeda;
 
+/*!
+    \class Andromeda::GeneralSettingsWidget
+    Widget that displays main Andromeda settings.
+*/
+
+/*!
+    Creates GeneralSettingsWidget with the given \a parent.
+*/
 GeneralSettingsWidget::GeneralSettingsWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GeneralSettingsWidget)
@@ -16,11 +24,17 @@ GeneralSettingsWidget::GeneralSettingsWidget(QWidget *parent) :
     connect(ui->showTrayIcon, SIGNAL(toggled(bool)), SLOT(onShowTrayIconChecked(bool)));
 }
 
+/*!
+    Destroys GeneralSettingsWidget.
+*/
 GeneralSettingsWidget::~GeneralSettingsWidget()
 {
     delete ui;
 }
 
+/*!
+    \internal
+*/
 void GeneralSettingsWidget::onShowTrayIconChecked(bool checked)
 {
     qApp->setTrayIconVisible(checked);
