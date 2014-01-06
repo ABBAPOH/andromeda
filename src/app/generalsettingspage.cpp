b5c1854cd4d287030fab2aeba37e7e941029ae84
Ivan Komissarov <ABBAPOH@gmail.com>
2014-01-06 22:56:43 +0400

    Add GeneralSettingsPage doc.

--- src/app/generalsettingspage.cpp
+++ src/app/generalsettingspage.cpp
@@ -4,31 +4,54 @@
 
 using namespace Andromeda;
 
+/*!
+    \class Andromeda::GeneralSettingsPage
+    Implements Parts::SettingsPage to create GeneralSettingsWidget.
+*/
+
+/*!
+    Creates GeneralSettingsPage with the given \a parent.
+*/
 GeneralSettingsPage::GeneralSettingsPage(QObject *parent) :
     SettingsPage(QLatin1String("1.General"), QLatin1String(" General"), parent)
 {
 }
 
+/*!
+    \reimp
+*/
 QString Andromeda::GeneralSettingsPage::name() const
 {
     return tr("General");
 }
 
+/*!
+    \reimp
+*/
 QIcon GeneralSettingsPage::icon() const
 {
     return QIcon(":/andromeda/icons/andromeda.png");
 }
 
+/*!
+    \reimp
+*/
 QString GeneralSettingsPage::categoryName() const
 {
     return tr("General");
 }
 
+/*!
+    \reimp
+*/
 QIcon GeneralSettingsPage::categoryIcon() const
 {
     return QIcon(":/andromeda/icons/andromeda.png");
 }
 
+/*!
+    \reimp
+*/
 QWidget * GeneralSettingsPage::createPage(QWidget *parent)
 {
     return new GeneralSettingsWidget(parent);
